6fc553059ba27b8d9cf6bcfaf6c234adb08100e4
yaoshanliang <1329517386@qq.com>
2015-06-27 10:34:54 +0800

    修改邮件配置

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,8 @@ return [
 	|
 	*/
 
-	'from' => ['address' => null, 'name' => null],
+	// 'from' => ['address' => null, 'name' => null],
+	'from' => ['address' => env('MAIL_USERNAME'), 'name' => env('MAIL_USERNAME')],
 
 	/*
 	|--------------------------------------------------------------------------
