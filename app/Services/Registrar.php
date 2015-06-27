023f8b0816b3a704b382423354771e8b9123046e
yaoshanliang <1329517386@qq.com>
2015-06-27 10:35:35 +0800

    用户名唯一认证

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -15,7 +15,7 @@ class Registrar implements RegistrarContract {
 	public function validator(array $data)
 	{
 		return Validator::make($data, [
-			'name' => 'required|max:255',
+			'name' => 'required|max:255|unique:users|min:5',
 			'email' => 'required|email|max:255|unique:users',
 			'password' => 'required|confirmed|min:6',
 		]);
