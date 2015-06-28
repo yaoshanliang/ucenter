c81f4f75a2e07ace058f5ed8918b2ddd8a68f441
yaoshanliang <1329517386@qq.com>
2015-06-28 19:24:39 +0800

    注册时改为username

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -15,7 +15,7 @@ class Registrar implements RegistrarContract {
 	public function validator(array $data)
 	{
 		return Validator::make($data, [
-			'name' => 'required|max:255|unique:users|min:5',
+			'username' => 'required|max:255|unique:users|min:5',
 			'email' => 'required|email|max:255|unique:users',
 			'password' => 'required|confirmed|min:6',
 		]);
@@ -30,7 +30,7 @@ class Registrar implements RegistrarContract {
 	public function create(array $data)
 	{
 		return User::create([
-			'name' => $data['name'],
+			'username' => $data['username'],
 			'email' => $data['email'],
 			'password' => bcrypt($data['password']),
 		]);
