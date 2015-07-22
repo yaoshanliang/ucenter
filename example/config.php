69285cfb08e67e6453cb1e653e3224ef5edb8586
yaoshanliang <1329517386@qq.com>
2015-07-22 16:21:44 +0800

    update example

--- example/config.php
+++ example/config.php
@@ -1,6 +1,6 @@
 <?php
 define('app', 'example');//应用名称，需要申请
 define('app_secret', 'example_secret');//应用密钥，需要申请
-define('site_home', 'http://localhost/ids/example');//本站地址
+define('site_home', 'http://example.com');//本站地址
 define('ids_home', 'http://ids.com');//统一身份认证地址
 ?>
--- example/index.php
+++ example/index.php
@@ -5,9 +5,9 @@ echo '<b>示例应用首页</b>', '<br />';
 if(isset($_SESSION['username'])) {
 	//自行登录成功后的处理
 	echo $_SESSION['username'], ' login', '<br />';
-	echo '<a href="http://localhost/ids/example/logout.php">退出</a>';
+	echo '<a href=' . site_home . '/logout.php>退出</a>';
 } else {
 	echo '<a href=' . ids_home . '/auth/login?app=' . app . '>统一身份认证界面登录</a>', '<br />';
-	echo '<a href=' . site_home .'/login_form.php>自定义界面登录</a>';
+	echo '<a href=' . site_home . '/login_form.php>自定义界面登录</a>';
 }
 ?>
