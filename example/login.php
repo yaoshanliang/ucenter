37f390ed436ab8be8d9100f2c18a940800265fcb
yaoshanliang <1329517386@qq.com>
2015-07-22 17:48:11 +0800

    重写token

--- example/login.php
+++ example/login.php
@@ -5,13 +5,26 @@ if(isset($_SESSION['username'])) {
 	header('Location:' . site_home);
 	exit;
 } else {
-	$validate_token = MD5($_GET['username'] . app_secret);
-	if($validate_token === $_GET['token']) {
-		$_SESSION['username'] = $_GET['username'];
-		header('Location:' . site_home);
-	} else {
+	$data['request_type'] = 'login';
+	$data['data']['token'] = $_GET['token'];
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, ids_home . '/api');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	curl_setopt($ch, CURLOPT_POST, 1);
+
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+	$output = curl_exec($ch);
+	curl_close($ch);
+	$result = json_decode($output, true);
+
+	if($result['errCode'] !== 0) {
 		// echo 'token 不合法！';//上线时不显示
 		echo '登录失败';
+	} else {
+		$_SESSION['username'] = $result['data']['username'];
+		header('Location:' . site_home);
 	}
 }
 ?>
