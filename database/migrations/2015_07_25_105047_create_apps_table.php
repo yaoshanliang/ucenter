65e2f867964836c801ee461d312a758739c97ce5
yaoshanliang <1329517386@qq.com>
2015-07-25 10:57:08 +0800

    添加 apps 数据库迁移文件

---
+++ database/migrations/2015_07_25_105047_create_apps_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('apps', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('app')->unique();
+			$table->string('app_name');
+			$table->string('app_secret');
+			$table->string('app_home_url');
+			$table->string('app_login_url');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('apps');
+	}
+
+}
