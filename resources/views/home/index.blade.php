9bf6fcb8d0ad63522a8aa969963721ce1c814842
yaoshanliang <1329517386@qq.com>
2015-07-27 21:45:18 +0800

    增加home页面

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,17 @@
+@extends('home.base')
+
+@section('content')
+<!--<div class="container">-->
+<!--  <div class="row">-->
+<!--    <div class="col-md-10 col-md-offset-1">-->
+      <div class="panel panel-default">
+        <div class="panel-heading">后台首页</div>
+
+        <div class="panel-body">
+
+        </div>
+      </div>
+<!--    </div>-->
+<!--  </div>-->
+<!--</div>-->
+@endsection
