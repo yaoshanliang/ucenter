5b5ae1ce8e77a01f69a01c39f8f397f52bbe368f
yaoshanliang <1329517386@qq.com>
2015-07-28 21:52:35 +0800

    修改welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,17 @@
-@extends('base')   {{-- 继承master模版 --}}
+@extends('app')
 
-@section('title')   {{-- 对应@yield('title') --}}
-    学生成绩管理系统
-@stop
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-default">
+				<div class="panel-heading">Home</div>
 
-@section('content')   {{-- 对应@yield('content') --}}
-    <div class="container">
-        <div class="jumbotron">
-            <h2><div class="quote">{{ Inspiring::quote() }}</div></h2>
-            <p>同学们登录后先修改相关资料</p>
-            <p>查询分数,有疑问咨询管理员</p>
-            <p><a class="btn btn-primary btn-lg" href="/auth/login" role="button">点击登录</a></p>
-        </div>
-    </div>
-@stop
+				<div class="panel-body">
+					You are logged in!
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
