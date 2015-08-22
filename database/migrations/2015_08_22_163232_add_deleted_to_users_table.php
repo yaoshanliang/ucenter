a87ae874ca9168efdd3b4ee6f5de280f17687fe4
yaoshanliang <1329517386@qq.com>
2015-08-22 16:37:47 +0800

    add deleted_at 软删除

---
+++ database/migrations/2015_08_22_163232_add_deleted_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDeletedToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
