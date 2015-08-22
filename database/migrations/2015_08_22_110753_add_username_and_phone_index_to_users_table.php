f789c7b87168fe2a1590dd05ad15205e7b5ac50c
yaoshanliang <1329517386@qq.com>
2015-08-22 11:36:34 +0800

    phone username字段增加索引

---
+++ database/migrations/2015_08_22_110753_add_username_and_phone_index_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsernameAndPhoneIndexToUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->unique('username');
+			$table->index('phone');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
