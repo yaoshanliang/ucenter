22c2c9fd869d06025a5a4a7fe528d647cc9b6060
yaoshanliang <1329517386@qq.com>
2015-08-24 23:30:23 +0800

    分离layout

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- jQuery -->
+<script src="/admin-assets/js/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/admin-assets/js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="/admin-assets/js/metisMenu.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="/admin-assets/js/sb-admin-2.js"></script>
---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,17 @@
+<!-- Bootstrap Core CSS -->
+<link href="/admin-assets/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- MetisMenu CSS -->
+<link href="/admin-assets/css/metisMenu.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="/admin-assets/css/sb-admin-2.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="/admin-assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<style>
+.page .pagination{
+	margin-top: 0px;
+	margin-bottom: 0px;
+}
+</style>
