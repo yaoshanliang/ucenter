56edfe3a5deb30375ea357f5a4421695541e3a9e
yaoshanliang <1329517386@qq.com>
2015-08-26 11:04:03 +0800

    setting表迁移文件

---
+++ database/migrations/2015_08_26_104945_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('settings', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name')->unique();
+			$table->string('value')->default('');
+			$table->string('description')->default($value);
+			$table->integer('order');
+			$table->enum('type', ['text', 'textarea', 'select', 'checkbox', 'radio'])->default('text');
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('settings');
+	}
+
+}
