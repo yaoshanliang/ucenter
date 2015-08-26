04ce18a6e67504caf770f2d480ea5ae296b195e5
yaoshanliang <1329517386@qq.com>
2015-08-26 11:55:33 +0800

    fillable

--- app/Setting.php
+++ app/Setting.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Setting extends Model {
 	protected $table = 'settings';
 
+	protected $fillable = ['id', 'name', 'value', 'description', 'type', 'order'];
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 }
