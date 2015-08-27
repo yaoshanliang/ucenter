3be30afebb858e0860d90319eb80b114f5a51ec4
yaoshanliang <1329517386@qq.com>
2015-08-27 22:27:06 +0800

    datatable本地样式

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -15,11 +15,45 @@
 <script src="/admin-assets/js/sb-admin-2.js"></script>
 
 <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<style>
+td.highlight {
+    background-color: whitesmoke !important;
+}
+</style>
 <script>
     $(document).ready(function() {
         $('#dataTables-example').DataTable({
+				"dom": '<"top"fi>rt<"bottom"lp><"clear">',
+				"pagingType":   "full_numbers",
+				"language": {
+					"lengthMenu": "每页 _MENU_ 条记录",
+					"zeroRecords": "没有找到记录",
+					"info": "第 _PAGE_ 页 ( 总共 _PAGES_ 页 )",
+					"infoEmpty": "无记录",
+					"infoFiltered": "(从 _MAX_ 条记录过滤)",
+					"search":"搜索：",
+					"paginate":{
+						"first":"首页",
+						"previous":"上一页",
+						"next":"下一页",
+						"last":"尾页"
+					}
+				},
                 responsive: true
         });
+		var lastIdx = null;
+    var table = $('#dataTables-example').DataTable();
+    $('#dataTables-example tbody')
+        .on( 'mouseover', 'td', function () {
+            var colIdx = table.cell(this).index().column;
+            if ( colIdx !== lastIdx ) {
+                $( table.cells().nodes() ).removeClass( 'highlight' );
+                $( table.column( colIdx ).nodes() ).addClass( 'highlight' );
+            }
+        } )
+        .on( 'mouseleave', function () {
+            $( table.cells().nodes() ).removeClass( 'highlight' );
+        } );
     });
 </script>
 
