77d390f1b7391bb57d93f4d53b0e45879d74ee7f
yaoshanliang <1329517386@qq.com>
2015-08-27 15:45:49 +0800

    增加datatables js和css

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -7,5 +7,19 @@
 <!-- Metis Menu Plugin JavaScript -->
 <script src="/admin-assets/js/metisMenu.min.js"></script>
 
+<!-- DataTables JavaScript -->
+<script src="/sb-admin/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="/sb-admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+
 <!-- Custom Theme JavaScript -->
 <script src="/admin-assets/js/sb-admin-2.js"></script>
+
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+                responsive: true
+        });
+    });
+</script>
+
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -4,11 +4,18 @@
 <!-- MetisMenu CSS -->
 <link href="/admin-assets/css/metisMenu.min.css" rel="stylesheet">
 
+<!-- Custom Fonts -->
+<link href="/admin-assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<!-- DataTables CSS -->
+<link href="/sb-admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+
+<!-- DataTables Responsive CSS -->
+<link href="/sb-admin/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+
 <!-- Custom CSS -->
 <link href="/admin-assets/css/sb-admin-2.css" rel="stylesheet">
 
-<!-- Custom Fonts -->
-<link href="/admin-assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 <style>
 .page .pagination{
 	margin-top: 0px;
