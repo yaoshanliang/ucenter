44972c6bda81e1fd8ee4d203df1b9345f7489fa2
yaoshanliang <1329517386@qq.com>
2015-08-30 16:28:26 +0800

    update  header css link

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -5,10 +5,10 @@
 <link href="/admin-assets/css/metisMenu.min.css" rel="stylesheet">
 
 <!-- DataTables CSS -->
-<link href="/sb-admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
+<link href="/plugin/sb-admin/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
 
 <!-- DataTables Responsive CSS -->
-<link href="/sb-admin/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
+<link href="/plugin/sb-admin/bower_components/datatables-responsive/css/dataTables.responsive.css" rel="stylesheet">
 
 <!-- Custom CSS -->
 <link href="/admin-assets/css/sb-admin-2.css" rel="stylesheet">
@@ -16,9 +16,20 @@
 <!-- Custom Fonts -->
 <link href="/admin-assets/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+<link href="/plugin/icheck/skins/square/blue.css" rel="stylesheet">
 <style>
 .page .pagination{
 	margin-top: 0px;
 	margin-bottom: 0px;
+}
+.checkbox, .radio {
+  display: block;
+width: 24px;
+  height: 24px;
+  line-height: 24px;
+  margin-right: 20px;
+border-radius: 0px;
+border: none;
+  cursor: pointer;
 }
 </style>
