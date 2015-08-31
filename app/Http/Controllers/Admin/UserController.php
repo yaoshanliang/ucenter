42c68f659a49e83817d2845e7e0ab8cb7012cf75
yaoshanliang <1329517386@qq.com>
2015-08-31 22:54:27 +0800

    commit list

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -22,6 +22,100 @@ class UserController extends Controller {
 		return view('admin.user.index', compact('users'));
 	}
 
+	public function getDatatable()
+	{
+		echo '111';exit;
+		// if(Datatable::shouldHandle()) {
+			return Datatable::collection(User::all(array('id','name')))
+				->showColumns('id', 'name')
+				->searchColumns('name')
+				->orderColumns('id','name')
+				->make();
+		// }
+	}
+
+	public function lists()
+	{
+		// var_dump($_POST);
+		// exit;
+		$draw = $_POST['draw'];//这个值作者会直接返回给前台
+
+		//排序
+		$columns = $_POST['columns'];
+		$order_column = $_POST['order']['0']['column'];//那一列排序，从0开始
+		$order_dir = $_POST['order']['0']['dir'];//ase desc 升序或者降序]
+		$orderSql = '';
+		if(isset($order_column)) {
+			$orderSql = ' order by ' . $columns[$order_column]['data'] . ' ' . $order_dir;
+		}
+		$search = $_POST['search']['value'];//获取前台传过来的过滤条件
+
+		$start = $_POST['start'];//从多少开始
+		$length = $_POST['length'];//数据长度
+		$limitSql = '';
+		$limitFlag = isset($_POST['start']) && $length != -1 ;
+		if ($limitFlag ) {
+		    $limitSql = " LIMIT ".intval($start).", ".intval($length);
+		}
+
+		//定义查询数据总记录数sql
+		$sumSql = "SELECT count(id) as sum FROM `users`";
+		//条件过滤后记录数 必要
+		$recordsFiltered = 0;
+		//表的总记录数 必要
+		$recordsTotal = 0;
+		// $recordsTotalResult = $db->query($sumSql);
+		// while ($row = $recordsTotalResult->fetchArray(SQLITE3_ASSOC)) {
+		   // $recordsTotal =  $row['sum'];
+		   // }
+		$sumSqlWhere =" where first_name||last_name||position||office||start_date||salary LIKE '%".$search."%'";
+		if(strlen($search)>0){
+			// $recordsFilteredResult = $db->query($sumSql.$sumSqlWhere);
+			// while ($row = $recordsFilteredResult->fetchArray(SQLITE3_ASSOC)) {
+					// $recordsFiltered =  $row['sum'];
+				// }
+		}else{
+		    $recordsFiltered = $recordsTotal;
+		}
+		//分页
+		$start = $_POST['start'];//从多少开始
+		$length = $_POST['length'];//数据长度
+		$limitSql = '';
+		$limitFlag = isset($_POST['start']) && $length != -1 ;
+		if ($limitFlag ) {
+		   $limitSql = " LIMIT ".intval($start).", ".intval($length);
+		}
+		$users = User::where("username" , 'LIKE',  '%' . $search . '%')
+			->orWhere("email" , 'LIKE',  '%' . $search . '%')
+			->orWhere("phone" , 'LIKE',  '%' . $search . '%')
+			->orderby('updated_at', 'desc')
+			->get(array('id', 'username', 'email', 'phone', 'created_at', 'updated_at'))
+			->toArray();
+		// $users = User::orderBy('updated_at', 'desc')->paginate(5);
+		// $users = User::get(array('id', 'username', 'email', 'phone', 'created_at', 'updated_at'))->toArray();
+		// foreach($users as $user) {
+			// $users_value[] = array_values($user);
+		// }
+// var_dump($users);
+// exit;
+		// var_dump($users_value);
+		// exit;
+		// var_dump(compact('users'));
+		// exit;
+		$draw = 1;
+		$recordsTotal = 57;
+		$recordsFiltered = 57;
+		$output['aaData'] = $users;
+		echo json_encode(array(
+		    "draw" => intval($draw),
+		    "recordsTotal" => intval($recordsTotal),
+		    "recordsFiltered" => intval($recordsFiltered),
+		    "data" => $users
+		),JSON_UNESCAPED_UNICODE);
+		// return json_encode($output);
+		// var_dump($_REQUEST);
+		// exit;
+	}
 	/**
 	 * Show the form for creating a new resource.
 	 *
