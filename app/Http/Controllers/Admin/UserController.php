79d76cbfe6d7b01d963d04e07048beb15fb7b537
yaoshanliang <1329517386@qq.com>
2015-09-03 18:59:32 +0800

    整理

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -17,42 +17,16 @@ class UserController extends Controller {
 	 */
 	public function index()
 	{
-		// $count = User::count();
-		// $users = User::orderBy('updated_at', 'desc')->paginate(5);
-		// return view('admin.user.index', compact('users'));
 		return view('admin.user.index');
 	}
 
 	public function lists()
 	{
-		$draw = $_POST['draw']+1;//这个值作者会直接返回给前台
-
 		//排序
 		$columns = $_POST['columns'];
 		$order_column = $_POST['order']['0']['column'];//那一列排序，从0开始
 		$order_dir = $_POST['order']['0']['dir'];//ase desc 升序或者降序]
-		$orderSql = '';
-		if(isset($order_column)) {
-			$orderSql = ' order by ' . $columns[$order_column]['data'] . ' ' . $order_dir;
-		}
-		// echo $orderSql;
 		$search = $_POST['search']['value'];//获取前台传过来的过滤条件
-
-		$start = $_POST['start'];//从多少开始
-		$length = $_POST['length'];//数据长度
-		$limitSql = '';
-		$limitFlag = isset($_POST['start']) && $length != -1 ;
-		if ($limitFlag ) {
-		    $limitSql = " LIMIT ".intval($start).", ".intval($length);
-		}
-
-		//定义查询数据总记录数sql
-		$sumSql = "SELECT count(id) as sum FROM `users`";
-		//条件过滤后记录数 必要
-		$recordsFiltered = 0;
-		//表的总记录数 必要
-		$recordsTotal = 0;
-		//分页
 		$start = $_POST['start'];//从多少开始
 		$length = $_POST['length'];//数据长度
 		$users = User::where("username" , 'LIKE',  '%' . $search . '%')
@@ -69,20 +43,17 @@ class UserController extends Controller {
 		} else {
 			$recordsFiltered = $recordsTotal;
 		}
-		$draw = $_POST['draw'];
 		$jsonp = preg_match('/^[$A-Z_][0-9A-Z_$]*$/i', $_GET['callback']) ? $_GET['callback'] : false;
-		if ( $jsonp ) {
+		if($jsonp) {
 		    echo $jsonp.'('.json_encode(array(
-				"draw" => intval($draw),
+				"draw" => intval($_POST['draw']),
 				"recordsTotal" => intval($recordsTotal),
 				"recordsFiltered" => intval($recordsFiltered),
 				"data" => $users
 				),JSON_UNESCAPED_UNICODE) . ');';
 		}
-		// return json_encode($output);
-		// var_dump($_REQUEST);
-		// exit;
 	}
+
 	/**
 	 * Show the form for creating a new resource.
 	 *
