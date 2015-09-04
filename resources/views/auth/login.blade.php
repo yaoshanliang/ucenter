d8923d457b3b25dba2ca5238e9a14d4fc9efb594
yaoshanliang <1329517386@qq.com>
2015-09-04 15:50:05 +0800

    记住我

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
-										<input type="checkbox" name="remember"> 记住用户名
+										<input type="checkbox" name="remember"> 记住我
 									</label>
 								</div>
 							</div>
