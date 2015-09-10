15ebfd563a5d3dff38c41bf665265a32333bba7d
yaoshanliang <1329517386@qq.com>
2015-09-10 16:56:53 +0800

    add Helper 类

---
+++ app/Services/Helper.php
@@ -0,0 +1,18 @@
+<?php namespace App\Services;
+class Helper {
+	/**
+	 * 返回jsonp格式数据
+	 *
+	 * @param  string $status_code, string $status_msg, string $status_data
+	 * @return jsonp格式字符串
+	 */
+	public static function jsonp_return($status_code, $status_msg, $status_data) {
+		$data['status_code'] = $status_code;
+		$data['status_msg'] = $status_msg;
+		$data['sttaus_data'] = $status_data;
+		$jsonp = preg_match('/^[$A-Z_][0-9A-Z_$]*$/i', $_GET['callback']) ? $_GET['callback'] : false;
+		if($jsonp) {
+		    echo $jsonp . '(' . json_encode($data, JSON_UNESCAPED_UNICODE) . ');';
+		}
+	}
+}
