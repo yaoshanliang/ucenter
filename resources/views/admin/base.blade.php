2cc7708d85bd84974fdd74f15b6b377007796907
yaoshanliang <1329517386@qq.com>
2015-09-12 21:40:13 +0800

    cache base

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -4,17 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>统一身份认证</title>
+	<title>{{ Cache::get('settings:site_name') }}</title>
 
-	<!-- Fonts -->
-	<!--<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>-->
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 	@include('admin.partials.header')
 	@yield('header')
 </head>
@@ -56,6 +47,5 @@
 	@include('admin.partials.footer')
 	@yield('footer')
 </body>
-
 </html>
 
