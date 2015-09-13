0947f70a8137c439fb46c172c9f5bd5b51e474e9
yaoshanliang <1329517386@qq.com>
2015-09-13 21:07:01 +0800

    update index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,8 @@
 @extends('admin.base')
 
 @section('content')
-<!--<div class="container">-->
-<!--  <div class="row">-->
-<!--    <div class="col-md-10 col-md-offset-1">-->
-      <div class="panel panel-default">
-        <div class="panel-heading">后台首页</div>
-
-        <div class="panel-body">
-
-        </div>
-      </div>
-<!--    </div>-->
-<!--  </div>-->
-<!--</div>-->
+<div class="panel panel-default">
+	<div class="panel-heading">后台首页</div>
+	<div class="panel-body"></div>
+</div>
 @endsection
