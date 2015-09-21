3b69893c568220ff19838405e920710498f250b8
yaoshanliang <1329517386@qq.com>
2015-09-21 14:52:55 +0800

    add ip and index

--- database/migrations/2015_09_21_111515_create_sys_logs_table.php
+++ database/migrations/2015_09_21_111515_create_sys_logs_table.php
@@ -19,10 +19,14 @@ class CreateSysLogsTable extends Migration {
 			$table->string('level');
 			$table->string('message');
 			$table->text('context')->default('');
+			$table->string('ip')->default('');
 			$table->dateTime('created_at');
 
             $table->foreign('app_id')->references('id')->on('apps')
                 ->onUpdate('cascade')->onDelete('cascade');
+
+			$table->index('app_id');
+			$table->index('level');
 		});
 	}
 
