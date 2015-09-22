ab3ca0b0db0792561f5d83fb142b163e5645b9d2
yaoshanliang <1329517386@qq.com>
2015-09-22 15:54:10 +0800

    add send mail handler

---
+++ app/Handlers/Commands/SendEmailHandler.php
@@ -0,0 +1,30 @@
+<?php namespace App\Handlers\Commands;
+
+use App\Commands\SendEmail;
+
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendEmailHandler {
+
+	/**
+	 * Create the command handler.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Handle the command.
+	 *
+	 * @param  SendEmail  $command
+	 * @return void
+	 */
+	public function handle(SendEmail $command)
+	{
+		//
+	}
+
+}
