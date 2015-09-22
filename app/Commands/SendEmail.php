5ac127b8462804115fd13d708b2ec5c5310035cc
yaoshanliang <1329517386@qq.com>
2015-09-22 15:53:55 +0800

    add send mail

---
+++ app/Commands/SendEmail.php
@@ -0,0 +1,23 @@
+<?php namespace App\Commands;
+
+use App\Commands\Command;
+
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+
+class SendEmail extends Command implements ShouldBeQueued {
+
+	use InteractsWithQueue, SerializesModels;
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+}
