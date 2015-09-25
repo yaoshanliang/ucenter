30f1d177b12f829e03b7fc7b297be5b1b5e8a198
yaoshanliang <1329517386@qq.com>
2015-09-25 17:08:23 +0800

    增加ips字段

--- app/UserLog.php
+++ app/UserLog.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserLog extends Model {
 	protected $table = 'user_logs';
 
-	protected $fillable = ['id', 'app_id', 'user_id', 'type', 'object', 'data', 'sql', 'ip', 'created_at'];
+	protected $fillable = ['id', 'app_id', 'user_id', 'type', 'object', 'data', 'sql', 'ip', 'ips', 'created_at'];
 
 	public $timestamps = false;
 
