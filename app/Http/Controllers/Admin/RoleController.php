53afa74e1e4e24ecc825b88877a52940b965ae8a
yaoshanliang <1329517386@qq.com>
2015-10-29 15:48:38 +0800

    test role

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -1,10 +1,11 @@
 <?php namespace App\Http\Controllers\Admin;
 
+use Zizaco\Entrust\EntrustPermission;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-
+use App\Role;
 class RoleController extends Controller {
 
 	/**
@@ -14,7 +15,15 @@ class RoleController extends Controller {
 	 */
 	public function index()
 	{
-		//
+		// $owner = new Role();
+		// $owner->app_id = 2;
+		// $owner->name         = 'owner';
+		// $owner->title = 'Project Owner'; // optional
+		// $owner->description  = 'User is the owner of a given project'; // optional
+		// $owner->save();
+		$role = Role::find(12);
+		$role->delete();
+		// return view('admin.role.index');
 	}
 
 	/**
