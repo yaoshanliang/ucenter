9277f594c1fca6803f818a8b8f430ae2c5ed34a9
yaoshanliang <1329517386@qq.com>
2015-11-01 13:52:09 +0800

    remove is_admin in user

---
+++ database/migrations/2015_10_30_141504_remove_is_admin.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveIsAdmin extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('is_admin');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
