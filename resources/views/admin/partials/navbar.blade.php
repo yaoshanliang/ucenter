561837cb181cc37146030e1846d12e1ebcc09079
yaoshanliang <1329517386@qq.com>
2015-11-02 15:16:14 +0800

    add app and role in navbar

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -10,93 +10,25 @@
 </div>
 <!-- /.navbar-header -->
 
-<ul class="nav navbar-top-links navbar-right">
+<ul class="nav navbar-nav navbar-right">
 	<li class="dropdown">
-		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-			<i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
-		</a>
-		<ul class="dropdown-menu dropdown-messages">
-			<li>
-				<a href="#">
-					<div>
-						<strong>John Smith</strong>
-						<span class="pull-right text-muted">
-							<em>Yesterday</em>
-						</span>
-					</div>
-					<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-				</a>
-			</li>
-			<li class="divider"></li>
-			<li>
-				<a href="#">
-					<div>
-						<strong>John Smith</strong>
-						<span class="pull-right text-muted">
-							<em>Yesterday</em>
-						</span>
-					</div>
-					<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-				</a>
-			</li>
-			<li class="divider"></li>
-			<li>
-				<a class="text-center" href="#">
-					<strong>Read All Messages</strong>
-					<i class="fa fa-angle-right"></i>
-				</a>
-			</li>
-		</ul>
-		<!-- /.dropdown-messages -->
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Session::get('current_app_title') }} <span class="caret"></span></a>
+			<ul class="dropdown-menu" role="menu">
+				@foreach (Session::get('apps') as $app)
+					<li><a>{{ $app['title'] }}</a></li>
+					<li class="divider"></li>
+				@endforeach
+			</ul>
 	</li>
-	<!-- /.dropdown -->
 	<li class="dropdown">
-		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-			<i class="fa fa-tasks fa-fw"></i>  <i class="fa fa-caret-down"></i>
-		</a>
-		<ul class="dropdown-menu dropdown-tasks">
-			<li>
-				<a href="#">
-					<div>
-						<p>
-							<strong>Task 1</strong>
-							<span class="pull-right text-muted">40% Complete</span>
-						</p>
-						<div class="progress progress-striped active">
-							<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-								<span class="sr-only">40% Complete (success)</span>
-							</div>
-						</div>
-					</div>
-				</a>
-			</li>
-			<li class="divider"></li>
-			<li>
-				<a href="#">
-					<div>
-						<p>
-							<strong>Task 2</strong>
-							<span class="pull-right text-muted">20% Complete</span>
-						</p>
-						<div class="progress progress-striped active">
-							<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-								<span class="sr-only">20% Complete</span>
-							</div>
-						</div>
-					</div>
-				</a>
-			</li>
-			<li class="divider"></li>
-			<li>
-				<a class="text-center" href="#">
-					<strong>See All Tasks</strong>
-					<i class="fa fa-angle-right"></i>
-				</a>
-			</li>
-		</ul>
-		<!-- /.dropdown-tasks -->
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Session::get('current_role_title') }} <span class="caret"></span></a>
+			<ul class="dropdown-menu" role="menu">
+				@foreach (Session::get('roles.' . Session::get('current_app_id')) as $role)
+					<li><a>{{ $role['title'] }}</a></li>
+					<li class="divider"></li>
+				@endforeach
+			</ul>
 	</li>
-	<!-- /.dropdown -->
 	<li class="dropdown">
 		<a class="dropdown-toggle" data-toggle="dropdown" href="#">
 			<i class="fa fa-bell fa-fw"></i>  <i class="fa fa-caret-down"></i>
