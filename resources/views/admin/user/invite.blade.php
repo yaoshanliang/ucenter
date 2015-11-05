73b0d5cf9b470934e57d86096ee29c5a1b5571ef
yaoshanliang <1329517386@qq.com>
2015-11-05 21:14:54 +0800

    add username when invite

--- resources/views/admin/user/invite.blade.php
+++ resources/views/admin/user/invite.blade.php
@@ -17,6 +17,16 @@
 			</div>
 
 			<div class="form-group">
+				<label class="col-md-3 control-label">用户名</label>
+				<div class="col-md-4">
+					<input type="text" class="form-control" name="username" value="{{ old('username') }}">
+				</div>
+				<div class="col-md-2 control-label">
+					（用户可修改）
+				</div>
+			</div>
+
+			<div class="form-group">
 				<div class="col-md-2 col-md-offset-3">
 					<button type="submit" class="btn btn-primary">
 						邀请加入
