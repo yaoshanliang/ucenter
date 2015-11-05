dcc921f272c9aff40833ca697daac09c36bbfab1
yaoshanliang <1329517386@qq.com>
2015-11-05 13:37:46 +0800

    update error list

--- resources/views/errors/list.blade.php
+++ resources/views/errors/list.blade.php
@@ -1,7 +1,10 @@
-@if ($errors->any())
-	<ul class="alert alert-danger">
-		@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
+@if (count($errors) > 0)
+	<div class="alert alert-danger">
+		<strong>Whoops!</strong> There were some problems with your input.<br><br>
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
 @endif
