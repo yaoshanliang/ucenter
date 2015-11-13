cea60af0ef875c59a258a5591833b593f8f7ff60
yaoshanliang <1329517386@qq.com>
2015-11-13 10:40:00 +0800

    add remove modal

--- resources/views/admin/partials/modal.blade.php
+++ resources/views/admin/partials/modal.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                <button type="button" class="btn btn-primary" onClick="return submit_delete(datatable_id, delete_url, $('#selected_ids').val());">确定</button>
+                <button type="button" class="btn btn-primary" onClick="return submit_delete(datatable_id, delete_url, delete_ids);">确定</button>
             </div>
         </div>
         <!-- /.modal-content -->
@@ -17,6 +17,26 @@
 </div>
 <!-- /.modal -->
 
+<!-- Modal -->
+<div class="modal fade" id="confirm_remove_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title" id="myModalLabel">确定移除吗？</h5>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                <button type="button" class="btn btn-primary" onClick="return submit_remove(datatable_id, remove_url, remove_ids);">确定</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- Modal -->
 <div class="modal fade" id="no_selected_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
