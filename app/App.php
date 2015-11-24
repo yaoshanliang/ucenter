83a59052370cecbe59c6c885a9a0ae626f5b0a3a
yaoshanliang <1329517386@qq.com>
2015-11-24 09:07:34 +0800

    add protected fillable

--- app/App.php
+++ app/App.php
@@ -8,4 +8,5 @@ class App extends Model {
 
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
+	protected $fillable = ['name', 'title', 'description', 'home_url', 'login_url', 'secret', 'user_id'];
 }
