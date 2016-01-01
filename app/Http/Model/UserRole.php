97967c49d9ed2dfcd6526c95d0b594eb68102e74
yaoshanliang <1329517386@qq.com>
2016-01-01 13:47:24 +0800

    mv model to model folder

---
+++ app/Http/Model/UserRole.php
@@ -0,0 +1,13 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+class UserRole extends Model {
+
+	use SoftDeletes;
+
+	protected $table = 'user_role';
+	protected $dates = ['deleted_at'];
+
+}
