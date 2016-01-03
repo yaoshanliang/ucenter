8f27e2a37418392b7b8fcdd1f3bb2786ae6b6312
yaoshanliang <1329517386@qq.com>
2016-01-03 18:06:31 +0800

    update getUserInfo

--- app/Model/User.php
+++ app/Model/User.php
@@ -8,33 +8,45 @@ use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 use Zizaco\Entrust\Traits\EntrustUserTrait;
+use Cache;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
-	use Authenticatable, CanResetPassword;
-	use EntrustUserTrait;
+    use Authenticatable, CanResetPassword;
+    use EntrustUserTrait;
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = ['username', 'email', 'password'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['username', 'email', 'password'];
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = ['password', 'remember_token'];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 
-	use SoftDeletes;
-	protected $dates = ['deleted_at'];
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
+
+    // 获取用户信息
+    protected function getUserInfo($user_id) {
+        $user_info = Cache::get(env('CACHE_USERS_PREFIX') . $user_id, function() use ($user_id) {
+            $user_info = User::findOrFail($user_id)->toArray();
+            Cache::forever(env('CACHE_USERS_PREFIX') . $user_id, $user_info);
+            return $user_info;
+        });
+        return $user_info;
+
+    }
 }
