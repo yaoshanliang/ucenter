6e7fd6b253ff17dda35dc54ad528d2f372c8d1d6
yaoshanliang <1329517386@qq.com>
2016-01-04 14:54:23 +0800

    update app

--- app/Http/Controllers/Admin/AppController.php
+++ app/Http/Controllers/Admin/AppController.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-use App\App;
+use App\Model\App;
 use Redirect, Input, Auth;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\DB;
+use App\Services\Helper;
+use Queue;
+use App\Jobs\UserLog;
 
 class AppController extends Controller {
 
@@ -19,10 +22,76 @@ class AppController extends Controller {
 	 */
 	public function index()
 	{
-		$count = App::count();
-		$apps = App::orderBy('updated_at', 'desc')->paginate(5);
-		return view('admin.app.index', compact('apps', 'count'));
-		return view('admin.app.index')->withApps(App::all());
+		return view('admin.app.index');
+	}
+
+	public function app()
+	{
+		return view('admin.app.app');
+	}
+
+	public function lists(Request $request)
+	{
+		$user_id = Auth::id();
+		$columns = $_POST['columns'];
+		$order_column = $_POST['order']['0']['column'];
+		$order_dir = $_POST['order']['0']['dir'];
+		$search = $_POST['search']['value'];
+		$start = $_POST['start'];
+		$length = $_POST['length'];
+		$fields = array('id', 'name', 'title', 'user_id', 'created_at', 'updated_at');
+		if(isset($_GET['type']) && $_GET['type'] == 'app') {
+			$recordsTotal = App::where('user_id', $user_id)->count();
+		} else {
+			$recordsTotal = App::count();
+		}
+		if(strlen($search)) {
+			$users = App::where(function ($query) use ($search, $user_id) {
+				if(isset($_GET['type']) && $_GET['type'] == 'app') {
+					$query->where('user_id', $user_id);
+				}
+				$query->where("name" , 'LIKE',  '%' . $search . '%')
+					->orWhere("title" , 'LIKE',  '%' . $search . '%');
+				})
+				->orderby($columns[$order_column]['data'], $order_dir)
+				->skip($start)
+				->take($length)
+				->get($fields)
+				->toArray();
+			$recordsFiltered = count($users);
+		} else {
+			$users = App::where(function ($query) use ($search, $user_id) {
+				if(isset($_GET['type']) && $_GET['type'] == 'app') {
+					$query->where('user_id', $user_id);
+				}})
+				->orderby($columns[$order_column]['data'], $order_dir)
+				->skip($start)
+				->take($length)
+				->get($fields)
+				->toArray();
+			$recordsFiltered = $recordsTotal;
+		}
+		// $query_log = DB::getQueryLog();
+		$ips = $request->ips();
+		$ip = $ips[0];
+		$ips = implode(',', $ips);
+		// foreach($query_log as $v) {
+			// $query_log_sql[]  = $v['query'];
+		// }
+		// $query_log_sql = implode(';', $query_log_sql);
+		// $log = Queue::push(new UserLog(1, Auth::user()->id, 'S', '用户', '', $query_log_sql, $ip, $ips));
+		$return_data = array(
+							"draw" => intval($_POST['draw']),
+							"recordsTotal" => intval($recordsTotal),
+							"recordsFiltered" => intval($recordsFiltered),
+							"data" => $users
+						);
+		$jsonp = preg_match('/^[$A-Z_][0-9A-Z_$]*$/i', $_GET['callback']) ? $_GET['callback'] : false;
+		if($jsonp) {
+		    echo $jsonp . '(' . json_encode($return_data, JSON_UNESCAPED_UNICODE) . ');';
+		} else {
+		    echo json_encode($return_data, JSON_UNESCAPED_UNICODE);
+		}
 	}
 
 	/**
@@ -56,12 +125,25 @@ class AppController extends Controller {
 			'home_url' => $request->home_url,
 			'login_url' => $request->login_url,
 			'secret' => $request->secret,
-			'user_id' => Auth::user()->id
+			'user_id' => Auth::id()
 		));
 
+        // 接入oauth_clients
+        DB::table('oauth_clients')->insert(array(
+            'id' => $request->name,
+            'secret' => $request->secret,
+            'name' => $request->title,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ));
+
+		$ips = $request->ips();
+		$ip = $ips[0];
+		$ips = implode(',', $ips);
+		$log = Queue::push(new UserLog(1, Auth::id(), 'A', '新增应用', 'name : ' . $request->name . '; title : ' . $request->title, '', $ip, $ips));
 		if ($app) {
 			session()->flash('success_message', '应用添加成功');
-			return Redirect::to('admin/app');
+			return Redirect::to('/admin/app/app');
 		} else {
 			return Redirect::back()->withInput()->withErrors('保存失败！');
 		}
@@ -99,10 +181,6 @@ class AppController extends Controller {
 	{
 		$this->validate($request, [
 			'name' => 'required|unique:apps,name,'.$id.'',
-			// 'title' => 'required',
-			// 'home_url' => 'required|url',
-			// 'login_url' => 'required|url',
-			// 'secret' => 'required'
 		]);
 
 		$app = App::where('id', $id)->update(array('name' => $request->name,
@@ -114,9 +192,16 @@ class AppController extends Controller {
 			'user_id' => Auth::user()->id
 		));
 
+        DB::table('oauth_clients')->where('id', $id)->update(array(
+            'id' => $request->name,
+            'secret' => $request->secret,
+            'name' => $request->title,
+            'updated_at' => date('Y-m-d H:i:s')
+        ));
+
 		if ($app) {
 			session()->flash('success_message', '应用修改成功');
-			return Redirect::to('admin/app');
+			return Redirect::to('/admin/app/app');
 		} else {
 			return Redirect::back()->withInput()->withErrors('保存失败！');
 		}
@@ -130,11 +215,26 @@ class AppController extends Controller {
 	 */
 	public function destroy($id)
 	{
-		$App = App::find($id);
-		$App->delete();
+		return false;
+	}
 
-		session()->flash('message', '应用删除成功');
-		return Redirect::to('admin/app');
+	//删除
+	public function delete()
+	{
+		DB::beginTransaction();
+		try {
+			$ids = $_POST['ids'];
+			Auth::user()->can('delete-all-app');
+			$result = App::whereIn('id', $ids)->delete();
+
+            DB::table('oauth_clients')->where('id', $id)->delete();
+			DB::commit();
+			Helper::jsonp_return(0, '删除成功', array('deleted_num' => $result));
+		} catch (Exception $e) {
+			DB::rollBack();
+			throw $e;
+			Helper::jsonp_return(1, '删除失败', array('deleted_num' => 0));
+		}
 	}
 
 }
