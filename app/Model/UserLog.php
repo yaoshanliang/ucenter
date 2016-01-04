9f171cbcca31eb8cb97dcbe19fcdbf986fff1645
yaoshanliang <1329517386@qq.com>
2016-01-04 16:45:48 +0800

    update fillable in user log

--- app/Model/UserLog.php
+++ app/Model/UserLog.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserLog extends Model {
 	protected $table = 'user_logs';
 
-	protected $fillable = ['id', 'app_id', 'user_id', 'type', 'object', 'data', 'sql', 'ip', 'ips', 'created_at'];
+	protected $fillable = ['id', 'app_id', 'user_id', 'type', 'title', 'data', 'sql', 'ip', 'ips', 'pushed_at', 'created_at'];
 
 	public $timestamps = false;
 
