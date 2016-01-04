49ecdb0d501af104b4aca35768bc6f88941e767e
yaoshanliang <1329517386@qq.com>
2016-01-04 16:44:27 +0800

    rename object to title

---
+++ database/migrations/2016_01_04_161122_update_object_to_title_to_user_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateObjectToTitleToUserLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_logs', function (Blueprint $table) {
+            $table->dropColumn('object');
+            $table->string('title')->default('')->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_logs', function (Blueprint $table) {
+            //
+        });
+    }
+}
