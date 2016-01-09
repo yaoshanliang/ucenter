67b726253ea93581b958215a7f3f3a6fc8bb108d
yaoshanliang <1329517386@qq.com>
2016-01-09 22:04:01 +0800

    retab!

--- resources/views/admin/user/app.blade.php
+++ resources/views/admin/user/app.blade.php
@@ -4,41 +4,41 @@
 <div class="row">
 <div class="col-lg-12">
 <div class="panel panel-default">
-	<div class="panel-heading">用户管理</div>
-	<div class="panel-body">
-		<div class="dataTable_wrapper">
-			<br />
-			<div class="input-group custom-search-form">
-				<input type="text" id="search" class="form-control search" placeholder="搜索">
-				<span class="input-group-btn">
-					<button class="btn btn-default" type="button">
-						<i class="fa fa-search"></i>
-					</button>
-				</span>
-			</div>
-			<form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/user/appuser') }}">
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<input type="hidden" name="selected_ids" id="selected_ids">
-				<table class="table table-striped table-bordered table-hover" id="user_index" class="display" cellspacing="0" width="100%" border='0px'>
-					<thead>
-						<tr>
-							<td style="width:15px"><input class="checkbox" type="checkbox" name="id" id='checkAll'></td>
-							<td>用户名</td>
-							<td>邮箱</td>
-							<td>手机</td>
-							<td>创建时间</td>
-							<td>更新时间</td>
-							<td>操作</td>
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>
-				</table>
-			</form>
-		</div>
-		<!-- /.dataTable_wrapper -->
-	</div>
-	<!-- /.panel-body -->
+    <div class="panel-heading">用户管理</div>
+    <div class="panel-body">
+        <div class="dataTable_wrapper">
+            <br />
+            <div class="input-group custom-search-form">
+                <input type="text" id="search" class="form-control search" placeholder="搜索">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/user/appuser') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="selected_ids" id="selected_ids">
+                <table class="table table-striped table-bordered table-hover" id="user_index" class="display" cellspacing="0" width="100%" border='0px'>
+                    <thead>
+                        <tr>
+                            <td style="width:15px"><input class="checkbox" type="checkbox" name="id" id='checkAll'></td>
+                            <td>用户名</td>
+                            <td>邮箱</td>
+                            <td>手机</td>
+                            <td>创建时间</td>
+                            <td>更新时间</td>
+                            <td>操作</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+        <!-- /.dataTable_wrapper -->
+    </div>
+    <!-- /.panel-body -->
 </div>
 <!-- /.panel -->
 </div>
@@ -51,22 +51,22 @@ var order = [5, 'desc'];
 var ajax_url = '/admin/user/lists?type=appuser';
 var remove_url = '/admin/user/remove';
 var columns = [{
-					"data": "id",
-					"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-						$(nTd).html("<input type='checkbox' id='" + sData + "' class='checkbox' name='ids' value='" + sData + "'>");
-					}
-				},
-				{"data": "username"},
-				{"data": "email"},
-				{"data": "phone"},
-				{"data": "created_at"},
-				{"data": "updated_at"},
-				{
-					"data": "id",
-					"fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-						$(nTd).html("<a href=/admin/user/" + sData + "/edit>编辑</a>" + " " +
-							"<a href='javascript:void(0);' onclick='return check_remove(" + sData + ");'>移除</a>");
-					}
-				}];
+                    "data": "id",
+                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                        $(nTd).html("<input type='checkbox' id='" + sData + "' class='checkbox' name='ids' value='" + sData + "'>");
+                    }
+                },
+                {"data": "username"},
+                {"data": "email"},
+                {"data": "phone"},
+                {"data": "created_at"},
+                {"data": "updated_at"},
+                {
+                    "data": "id",
+                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                        $(nTd).html("<a href=/admin/user/" + sData + "/edit>编辑</a>" + " " +
+                            "<a href='javascript:void(0);' onclick='return check_remove(" + sData + ");'>移除</a>");
+                    }
+                }];
 </script>
 @endsection
--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -2,37 +2,37 @@
 
 @section('content')
 <div class="panel panel-default">
-	<div class="panel-heading">新增 用户</div>
-	<div class="panel-body">
-		@if (count($errors) > 0)
-			<div class="alert alert-danger">
-				<strong>Whoops!</strong> There were some problems with your input.<br><br>
-				<ul>
-					@foreach ($errors->all() as $error)
-						<li>{{ $error }}</li>
-					@endforeach
-				</ul>
-			</div>
-		@endif
+    <div class="panel-heading">新增 用户</div>
+    <div class="panel-body">
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-		<form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/user') }}">
-			<input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/user') }}">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-			<div class="form-group">
-				<label class="col-md-3 control-label">邮箱</label>
-				<div class="col-md-6">
-					<input type="text" class="form-control" name="app" value="{{ old('email') }}">
-				</div>
-			</div>
+            <div class="form-group">
+                <label class="col-md-3 control-label">邮箱</label>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" name="app" value="{{ old('email') }}">
+                </div>
+            </div>
 
-			<div class="form-group">
-				<div class="col-md-2 col-md-offset-3">
-					<button type="submit" class="btn btn-primary">
-						邀请加入
-					</button>
-				</div>
-			</div>
-		</form>
-	</div>
+            <div class="form-group">
+                <div class="col-md-2 col-md-offset-3">
+                    <button type="submit" class="btn btn-primary">
+                        邀请加入
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
