fb3d24e6139d82449bf121540300c6e783632b0e
yaoshanliang <1329517386@qq.com>
2016-01-10 19:40:22 +0800

    update userlog index

--- resources/views/admin/userlog/index.blade.php
+++ resources/views/admin/userlog/index.blade.php
@@ -4,16 +4,11 @@
 <div class="row">
 <div class="col-lg-12">
 <div class="panel panel-default">
-    <div class="panel-heading">用户管理</div>
+    <div class="panel-heading">用户日志</div>
     <div class="panel-body">
         <div class="dataTable_wrapper">
             <br />
             <div class="input-group custom-search-form">
-                <a href="{{ URL('admin/user/invite') }}" class="btn btn-primary">邀请</a>
-                &nbsp;
-                <a href="{{ URL('admin/user/create') }}" class="btn btn-primary">新增</a>
-                &nbsp;
-                <a href='javascript:void(0);' class="btn btn-primary btn-danger" onclick='return check_delete();'>删除</a>
                 <input type="text" id="search" class="form-control search" placeholder="搜索">
                 <span class="input-group-btn">
                     <button class="btn btn-default" type="button">
@@ -21,18 +16,18 @@
                     </button>
                 </span>
             </div>
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/app') }}">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/userlog') }}">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
                 <table class="table table-striped table-bordered table-hover" id="user_index" class="display" cellspacing="0" width="100%" border='0px'>
                     <thead>
                         <tr>
-                            <td style="width:15px"><input class="checkbox" type="checkbox" name="id" id='checkAll'></td>
                             <td>用户名</td>
-                            <td>邮箱</td>
-                            <td>手机</td>
-                            <td>创建时间</td>
-                            <td>更新时间</td>
-                            <td>操作</td>
+                            <td>类型</td>
+                            <td>说明</td>
+                            <td>数据</td>
+                            <td>ip</td>
+                            <td>触发时间</td>
+                            <td>写库时间</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -50,27 +45,18 @@
 
 <script>
 var datatable_id = 'user_index';
-var columnDefs_targets = [0, 6];
+var columnDefs_targets = [];
 var order = [5, 'desc'];
-var ajax_url = '/admin/user/lists';
+var ajax_url = '/admin/userlog/lists';
 var delete_url = '/admin/user/delete';
-var columns = [{
-                    "data": "id",
-                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                        $(nTd).html("<input type='checkbox' id='" + sData + "' class='checkbox' name='ids' value='" + sData + "'>");
-                    }
-                },
-                {"data": "username"},
-                {"data": "email"},
-                {"data": "phone"},
+var columns = [
+                {"data": "user_id"},
+                {"data": "type"},
+                {"data": "title"},
+                {"data": "data"},
+                {"data": "ip"},
+                {"data": "pushed_at"},
                 {"data": "created_at"},
-                {"data": "updated_at"},
-                {
-                    "data": "id",
-                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-                        $(nTd).html("<a href=/admin/user/" + sData + "/edit>编辑</a>" + " " +
-                            "<a href='javascript:void(0);' onclick='return check_delete(" + sData + ");'>删除</a>");
-                    }
-                }];
+                ];
 </script>
 @endsection
