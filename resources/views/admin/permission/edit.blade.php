614a5498acb4919c59cc1e4fa543debfdb52811f
yaoshanliang <1329517386@qq.com>
2016-01-11 11:25:36 +0800

    update edit

--- resources/views/admin/permission/edit.blade.php
+++ resources/views/admin/permission/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="panel panel-default">
-    <div class="panel-heading">编辑 接入应用</div>
+    <div class="panel-heading">编辑 接入权限</div>
     <div class="panel-body">
 
     @if (count($errors) > 0)
@@ -16,41 +16,27 @@
         </div>
     @endif
 
-    <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/app/'.$app->id) }}">
+    <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/app/'.$permission->id) }}">
         <input name="_method" type="hidden" value="PUT">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <div class="form-group">
                 <label class="col-md-3 control-label">代号</label>
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="app" value="{{ $app->app }}">
+                    <input type="text" class="form-control" name="name" value="{{ $permission->name }}">
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-md-3 control-label">名称</label>
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="app_name" value="{{ $app->app_name }}">
+                    <input type="text" class="form-control" name="title" value="{{ $permission->title }}">
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="col-md-3 control-label">首页地址</label>
+                <label class="col-md-3 control-label">描述</label>
                 <div class="col-md-6">
-                    <input type="text" class="form-control" name="app_home_url" value="{{ $app->app_home_url }}">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label class="col-md-3 control-label">登录地址</label>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" name="app_login_url" value="{{ $app->app_login_url }}">
-                </div>
-            </div>
-
-            <div class="form-group">
-                <label class="col-md-3 control-label">密钥</label>
-                <div class="col-md-6">
-                    <input type="text" class="form-control" name="app_secret" value="{{ $app->app_secret }}">
+                    <input type="text" class="form-control" name="description" value="{{ $permission->description }}">
                 </div>
             </div>
 
