f50aeaa53d9f82ff770617a554ed21548f42109d
yaoshanliang <1329517386@qq.com>
2016-01-24 14:41:15 +0800

    update role

--- app/Model/Role.php
+++ app/Model/Role.php
@@ -1,11 +1,36 @@
-<?php namespace App\Model;
+<?php
+namespace App\Model;
 
 use Zizaco\Entrust\EntrustRole;
-
 use Zizaco\Entrust\Traits\EntrustRoleTrait;
-class Role extends EntrustRole {
 
+class Role extends EntrustRole
+{
 	use EntrustRoleTrait;
+
     protected $fillable = ['app_id', 'name', 'title', 'description'];
 
+    public function scopeWhereDataTables($query, $post, $fields = array())
+    {
+		if (strlen($post['search']['value'])) {
+            $query->where(function ($query) use ($post, $fields) {
+                foreach ($fields as $k => $v) {
+                    if ($k == 0) {
+			            $query->where($v, 'LIKE',  '%' . $post['search']['value'] . '%');
+                    } else {
+                        $query->orWhere($v, 'LIKE',  '%' . $post['search']['value'] . '%');
+                    }
+                }
+            });
+        }
+        return $query;
+    }
+
+    public function scopeOrderByDataTables($query, $post)
+    {
+        foreach ($post['order'] as $k => $v) {
+            $query->orderBy($post['columns'][$v['column']]['data'], $v['dir']);
+        }
+        return $query;
+    }
 }
