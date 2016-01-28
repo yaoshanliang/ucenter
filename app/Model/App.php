41c8b1dca7ad3039bd8f3664e2b8760429b23d57
yaoshanliang <1329517386@qq.com>
2016-01-28 10:19:49 +0800

    rm secret and deleted_at

--- app/Model/App.php
+++ app/Model/App.php
@@ -2,14 +2,11 @@
 namespace App\Model;
 
 use App\Model\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class App extends Model
 {
 	protected $table = 'apps';
 
-	use SoftDeletes;
-	protected $dates = ['deleted_at'];
 	protected $fillable = ['name', 'title', 'description', 'home_url', 'login_url', 'secret', 'user_id'];
 
     public function client()
---
+++ database/migrations/2016_01_28_100653_rm_secret_deleted_in_app.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RmSecretDeletedInApp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('apps', function (Blueprint $table) {
+            $table->dropColumn('secret');
+            $table->dropColumn('deleted_at');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('apps', function (Blueprint $table) {
+            //
+        });
+    }
+}
