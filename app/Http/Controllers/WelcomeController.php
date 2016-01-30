1fc7b60b9182bed6d26fccfa677ee46aff0cace7
yaoshanliang <1329517386@qq.com>
2016-01-30 15:41:41 +0800

    update redirect to admin or home

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,40 +1,48 @@
-<?php namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
+
 use Auth;
+use Session;
 
-class WelcomeController extends Controller {
+class WelcomeController extends Controller
+{
 
-	/*
-	|--------------------------------------------------------------------------
-	| Welcome Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders the "marketing page" for the application and
-	| is configured to only allow guests. Like most of the other sample
-	| controllers, you are free to modify or remove it as you desire.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Welcome Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller renders the "marketing page" for the application and
+    | is configured to only allow guests. Like most of the other sample
+    | controllers, you are free to modify or remove it as you desire.
+    |
+    */
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		if(!Auth::user()->is_admin) {
-			return redirect('home');
-		}
-		return redirect('admin');
-	}
-
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $roles = Session::get('roles');
+        foreach ($roles as $key => $value) {
+            foreach ($value as $k => $v) {
+                if ($v['name'] == 'developer') {
+                    return redirect('/admin/index');
+                }
+            }
+        }
+        return redirect('/home');
+    }
 }
