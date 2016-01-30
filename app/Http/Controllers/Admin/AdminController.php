1dfc3855d8ddb3e2e28ca40968e62d0f0e006b44
yaoshanliang <1329517386@qq.com>
2016-01-31 00:56:01 +0800

    add redirect between index and forbidden

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -11,6 +11,8 @@ use App\Model\Role;
 use Redirect, Input, Auth;
 use Cache;
 use Session;
+use Config;
+
 class AdminController extends Controller {
 
 	public function __construct()
@@ -27,11 +29,6 @@ class AdminController extends Controller {
 		}
 	}
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
 	public function index()
 	{
         $apps = Session::get('apps');
@@ -39,104 +36,14 @@ class AdminController extends Controller {
 		return view('admin.index')->with(compact('apps', 'roles'));
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		return view('admin.app.create');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		$this->validate($request, [
-			'app' => 'required|unique:apps',
-			'app_name' => 'required',
-			'app_home_url' => 'required',
-			'app_login_url' => 'required',
-			'app_secret' => 'required'
-		]);
-
-		$app = new App;
-		$app->app = Input::get('app');
-		$app->app_name = Input::get('app_name');
-		$app->app_home_url = Input::get('app_home_url');
-		$app->app_login_url = Input::get('app_login_url');
-		$app->app_secret = Input::get('app_secret');
-
-		if ($app->save()) {
-			return Redirect::to('admin');
-		} else {
-			return Redirect::back()->withInput()->withErrors('保存失败！');
-		}
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		return view('admin.pages.edit')->withPage(Page::find($id));
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		$this->validate($request, [
-			'title' => 'required|unique:pages,title,'.$id.'|max:255',
-			'body' => 'required',
-		]);
-
-		$page = Page::find($id);
-		$page->title = Input::get('title');
-		$page->body = Input::get('body');
-		$page->user_id = 1;//Auth::user()->id;
-
-		if ($page->save()) {
-			return Redirect::to('admin');
-		} else {
-			return Redirect::back()->withInput()->withErrors('保存失败！');
-		}
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$page = Page::find($id);
-		$page->delete();
-
-		return Redirect::to('admin');
-	}
-
+    public function forbidden()
+    {
+        $apps = Session::get('apps');
+        $roles = Session::get('roles');
+        $currentRole = Session::get('current_role');
+        if (in_array($currentRole['name'], Config::get('entrust.admin_role'))) {
+            return redirect('/admin/index');
+        }
+		return view('admin.forbidden')->with(compact('apps', 'roles'));
+    }
 }
--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -3,6 +3,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Request;
+use Session;
+use Config;
 
 class Role
 {
@@ -28,10 +31,11 @@ class Role
      */
     public function handle($request, Closure $next)
     {
-        $roles = 'developer|admin';//角色为developer或admin的才可以访问/admin
-        if ($this->auth->guest() || !$request->user()->hasRole(explode('|', $roles))) {
-            // abort(503);
-            return redirect('/home');
+        $currentRole = Session::get('current_role');
+        if (!in_array($currentRole['name'], Config::get('entrust.admin_role'))) {
+            if (!Request::is('admin/forbidden')) {
+                return redirect('/admin/forbidden');
+            }
         }
         return $next($request);
     }
