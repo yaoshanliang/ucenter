246787cf15200741917caea0f8106f517fc2d57c
yaoshanliang <1329517386@qq.com>
2016-01-30 18:05:14 +0800

    add confirm

--- resources/views/admin/partials/modal.blade.php
+++ resources/views/admin/partials/modal.blade.php
@@ -38,13 +38,13 @@
 
 <!-- Modal -->
 <div class="modal fade" id="no_selected_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
+    <div class="modal-dialog modal-dialog-confirm">
+        <div class="modal-content modal-content-confirm">
+            <div class="modal-header modal-header-confirm">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h5 class="modal-title" id="myModalLabel">请先勾选数据</h5>
             </div>
-            <div class="modal-footer">
+            <div class="modal-footer modal-footer-confirm">
                 <button type="button" class="btn btn-default" data-dismiss="modal">确定</button>
             </div>
         </div>
