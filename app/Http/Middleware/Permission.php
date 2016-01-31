973dcd21365d5e92f684fe8828c7493b53b3f63a
yaoshanliang <1329517386@qq.com>
2016-01-31 22:43:03 +0800

    add perms

--- app/Http/Middleware/Permission.php
+++ app/Http/Middleware/Permission.php
@@ -2,6 +2,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Session;
+use Config;
 use Auth;
 
 class Permission
@@ -15,8 +17,14 @@ class Permission
      */
     public function handle($request, Closure $next)
     {
-        if (!Auth::user()->is_admin) {
-            return redirect('home');
+        if (Auth::user()->can('create-app')) {
+            dd(1);
+        }
+        $currentRole = Session::get('current_role');
+        if (!in_array($currentRole['name'], Config::get('entrust.admin_role'))) {
+            if (!Request::is('admin/forbidden')) {
+                return redirect('/admin/forbidden');
+            }
         }
         return $next($request);
     }
