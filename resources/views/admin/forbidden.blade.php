9f0dd7f8cd9ad018ec7f68af3a93d3b669487f77
yaoshanliang <1329517386@qq.com>
2016-01-31 14:12:50 +0800

    update forbidden

--- resources/views/admin/forbidden.blade.php
+++ resources/views/admin/forbidden.blade.php
@@ -1,4 +1,9 @@
 @extends('admin.base')
-
 @section('content')
+<div class="panel panel-default">
+	<div class="panel-heading">禁止访问</div>
+    <div class="panel-body">
+        <h4>当前角色无权限访问管理后台</h4>
+    </div>
+</div>
 @endsection
