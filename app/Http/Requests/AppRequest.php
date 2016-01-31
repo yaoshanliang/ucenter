4bc20aece5781848d8c82f6155d9d77765e47a83
yaoshanliang <1329517386@qq.com>
2016-01-31 22:41:58 +0800

    update apprequest

--- app/Http/Requests/AppRequest.php
+++ app/Http/Requests/AppRequest.php
@@ -1,51 +1,43 @@
-<?php namespace App\Http\Requests;
+<?php
+namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 use App\Model\App;
 use Auth;
+use Response;
 
-class AppRequest extends Request {
+class AppRequest extends Request
+{
+    private $id;
 
-	private $id;
+    public function __construct()
+    {
+        // $this->id = $this->route('app');
+        // $this->id = $_GET['app'];
+    }
 
-	public function __construct()
-	{
-		// $this->id = $this->route('app');
-		// $this->id = $_GET['app'];
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $this->id = $this->route('app');
+        if ($this->id) {
+            return App::where('id', $this->id)->where('user_id', Auth::id())->exists();
+        } else {
+            return true;
+        }
+    }
 
-	/**
-	 * Determine if the user is authorized to make this request.
-	 *
-	 * @return bool
-	 */
-	public function authorize()
-	{
-		$this->id = $this->route('app');
-		if($this->id) {
-			return App::where('id', $this->id)->where('user_id', Auth::id())->exists();
-		} else {
-			return true;
-		}
-	}
-
-	/**
-	 * Get the validation rules that apply to the request.
-	 *
-	 * @return array
-	 */
-	public function rules()
-	{
-		if($_POST) {
-			return [
-				// 'name' => 'required|unique:apps',
-				'title' => 'required',
-				'home_url' => 'required|url',
-				'login_url' => 'required|url',
-				'secret' => 'required'
-			];
-		} else {
-			return [];
-		}
-	}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [];
+    }
 }
