10d58dbfbaf124dbade335159315cdabf07c760e
yaoshanliang <1329517386@qq.com>
2016-02-09 17:28:10 +0800

    update index

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -10,6 +10,27 @@
         </div>
     </div>
     <div class="panel-body">
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+        </div>
+    @endif
+
+    <form class="form-horizontal" role="form" method="POST" action="">
+        <input name="_method" type="hidden" value="PUT">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="form-group">
+                <label class="col-md-3 control-label">用户名</label>
+                <div class="col-md-6">
+                    <p class="form-control-static">{{ $user->username }}</p>
+                </div>
+            </div>
+    </form>
     </div>
     <!-- /.panel-body -->
 </div>
