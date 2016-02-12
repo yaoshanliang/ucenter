316e8aae7b7b5948419a959a9109ea928785f002
yaoshanliang <1329517386@qq.com>
2016-02-12 11:04:12 +0800

    update user index

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -24,12 +24,20 @@
     <form class="form-horizontal" role="form" method="POST" action="">
         <input name="_method" type="hidden" value="PUT">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <div class="form-group">
+        <div class="form-group">
+            <div class="col-md-6">
                 <label class="col-md-3 control-label">用户名</label>
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <p class="form-control-static">{{ $user->username }}</p>
                 </div>
             </div>
+            <div class="col-md-6">
+                <label class="col-md-3 control-label">用户名</label>
+                <div class="col-md-3">
+                    <p class="form-control-static">{{ $user->username }}</p>
+                </div>
+            </div>
+        </div>
     </form>
     </div>
     <!-- /.panel-body -->
