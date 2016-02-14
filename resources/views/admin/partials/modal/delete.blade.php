a30aba458ef9b188af83757a4b5d8875928263ec
yaoshanliang <1329517386@qq.com>
2016-02-14 14:13:33 +0800

    split modal to delete and remove modal

---
+++ resources/views/admin/partials/modal/delete.blade.php
@@ -0,0 +1,36 @@
+<!-- Modal -->
+<div class="modal fade" id="confirm_delete_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-confirm">
+        <div class="modal-content modal-content-confirm">
+            <div class="modal-header modal-content-confirm">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title" id="myModalLabel">确定删除吗？</h5>
+            </div>
+            <div class="modal-footer modal-footer-confirm">
+                <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+                <button type="button" class="btn btn-primary" onClick="return submit_datatable('delete', datatable_id, delete_url, delete_ids);">确定</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="no_selected_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-confirm">
+        <div class="modal-content modal-content-confirm">
+            <div class="modal-header modal-header-confirm">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title" id="myModalLabel">请先勾选数据</h5>
+            </div>
+            <div class="modal-footer modal-footer-confirm">
+                <button type="button" class="btn btn-default" data-dismiss="modal">确定</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
