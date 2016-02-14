4b6822f18e211e65b9b750a5df4ceaa5df80dc1f
yaoshanliang <1329517386@qq.com>
2016-02-14 11:20:17 +0800

    add user details

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,4 +1,5 @@
-<?php namespace App\Http\Controllers;
+<?php
+namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
@@ -6,13 +7,17 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 
 use App\Model\App;
 use App\Model\Role;
+use App\Model\User;
 use Cache;
 use Config;
-abstract class Controller extends BaseController {
+use DB;
+abstract class Controller extends BaseController
+{
 
     use DispatchesCommands, ValidatesRequests;
 
-    function cacheApps($app_id) {
+    function cacheApps($app_id)
+    {
         $appsArray = App::all();
         foreach($appsArray as $v) {
             $apps[$v['id']] = Cache::get(Config::get('cache.apps') . $v['id'], function() use ($v) {
@@ -24,7 +29,8 @@ abstract class Controller extends BaseController {
         return $apps[$app_id];
     }
 
-    function cacheRoles($role_id) {
+    function cacheRoles($role_id)
+    {
         $rolesArray = Role::all();
         foreach($rolesArray as $v) {
             $roles[$v['id']] = Cache::get(Config::get('cache.roles') . $v['id'], function() use ($v) {
@@ -34,5 +40,33 @@ abstract class Controller extends BaseController {
             });
         }
         return $roles[$role_id];
+    }
+
+    function cacheUsers()
+    {
+        // 用户详细信息字段
+        $userFieldsArray = DB::table('user_fields')->get(array('id', 'name', 'title', 'description'));
+        foreach ($userFieldsArray as $v) {
+            $userFields[$v->id] = array('name' => $v->name, 'title' => $v->title);
+        }
+
+        // 用户基本信息，初始化详细信息
+        $usersArray = User::get(array('id', 'username', 'email', 'phone'))->toArray();
+        foreach ($usersArray as $v) {
+            $users[$v['id']] = array('user_id' => $v['id'], 'username' => $v['username'], 'email' => $v['email'], 'phone' => $v['phone']);
+            foreach ($userFields as $value) {
+                $users[$v['id']]['details'][$value['name']] = array('title' => $value['title'], 'value' => '');
+            }
+        }
+
+        // 用户详细信息
+        $userInfoArray = DB::table('user_info')->get(array('user_id', 'field_id', 'value'));
+        foreach ($userInfoArray as $v) {
+            $users[$v->user_id]['details'][$userFields[$v->field_id]['name']]['value'] = $v->value;
+        }
+
+        foreach($users as $v) {
+            Cache::forever(Config::get('cache.users') . $v['user_id'], $v);
+        }
     }
 }
