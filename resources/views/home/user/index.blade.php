1d423993cc2ca164dd1c8810c78c05fcfb6a4eac
yaoshanliang <1329517386@qq.com>
2016-02-15 11:10:15 +0800

    draft send code view

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -58,6 +58,89 @@
 function bindPhone() {
     $('#bind_phone').modal('show');
 }
+function sendCode() {
+    var phone = $('input[name="phone"]').val();
+    if (phone.length != 11) {
+        showFailTip('手机号不合法');
+        return false;
+    }
+
+    $.ajax({
+        // url: '/api/sms/sendCode',
+        url: '/api/user/user_info',
+        data: {'phone': phone, 'access_token': 'test'},
+        dataType: 'json',
+        headers: {
+            'X-CSRF-TOKEN': $('input[name="_token"]').val()
+        },
+        success: function(data) {
+            if(data['code'] === 1) {
+                showSuccessTip(data['message']);
+                var InterValObj;
+                var total_count = 60;//总倒计时秒数
+                var current_count;
+                current_count = total_count;
+                $("#send_code").val(current_count + "(s)后重发");
+                InterValObj = window.setInterval(setRemainTime, 1000);
+                $("#send_code").addClass('disabled');
+                $("#send_code").removeClass('btn-outline');
+                //计时器
+                function setRemainTime() {
+                    if (current_count == 0) {
+                        window.clearInterval(InterValObj);
+                        $("#send_code").removeClass("disabled");
+                        $("#send_code").addClass('btn-outline');
+                        $("#send_code").val("发送验证码");
+                    } else {
+                        current_count--;
+                        $("#send_code").val(current_count + "(s)后重发");
+                    }
+                }
+
+            } else {
+                showFailTip(data['message']);
+                return false;
+            }
+        },
+        error: function(data) {
+            showFailTip(data['message']);
+            return false;
+        },
+    });
+}
+
+function validateCode() {
+    var phone = $('input[name="phone"]').val();
+    if (phone.length != 11) {
+        showFailTip('手机号不合法');
+        return false;
+    }
+    var code = $('input[name="code"]').val();
+    if (code.length != 6) {
+        showFailTip('验证码不合法');
+        return false;
+    }
+    $.ajax({
+        url: url,
+        data: {'phone': phone, 'code': code},
+        dataType: 'json',
+        headers: {
+            'X-CSRF-TOKEN': $('input[name="_token"]').val()
+        },
+        success: function(data) {
+            if(data['code'] === 1) {
+                showSuccessTip(data['message']);
+            } else {
+                showFailTip(data['message']);
+                return false;
+            }
+        },
+        error: function(data) {
+            showFailTip(data['message']);
+            return false;
+        },
+    });
+}
 </script>
 <!-- Modal -->
 <div class="modal fade" id="bind_phone" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -79,12 +162,12 @@ function bindPhone() {
                         <input type="text" class="form-control" name="code" placeholder="验证码">
                     </div>
                     <div class="col-md-4">
-                        <button type="button" class="btn btn-outline btn-success">发送验证码</button>
+                        <input type="button" id="send_code" class="btn btn-outline btn-success" onClick="return sendCode();" value="发送验证码">
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="col-md-12">
-                        <button type="button" class="btn btn-primary btn-block">确认</button>
+                        <button type="button" class="btn btn-primary btn-block" onClick="return validateCode();">确认</button>
                     </div>
                 </div>
             </form>
