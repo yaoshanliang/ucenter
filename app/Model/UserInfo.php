4b8f38407bd5754a304556a69c85ab5e12cd36d6
yaoshanliang <1329517386@qq.com>
2016-02-16 17:10:11 +0800

    add UserInfo Model

---
+++ app/Model/UserInfo.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Model;
+
+use App\Model\Model;
+
+class UserInfo extends Model
+{
+	protected $table = 'user_info';
+    protected $fillable = ['user_id', 'field_id', 'value'];
+}
