6ceec6d64960cc464be58ce5a5f466e1c05eb55e
yaoshanliang <1329517386@qq.com>
2016-02-17 14:40:23 +0800

    update user

--- resources/views/home/user/index.blade.php
+++ resources/views/home/user/index.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
 <div class="col-lg-12">
 <div class="panel panel-default">
-    <div class="panel-heading">个人信息
+    <div class="panel-heading">基本信息
         <div class="pull-right">
             <i class="fa fa-user"></i>个人信息
         </div>
@@ -16,11 +16,21 @@
         <div class="form-group">
             <div class="col-md-4">
                 <label class="col-md-3 control-label">用户名</label>
-                <p class="form-control-static">{{ $user['username'] }}</p>
+                <p class="form-control-static">{{ $user['username'] }}
+                    <button type="button" class="btn btn-outline btn-primary btn-xs" onclick="editUsername();">修改</button>
+                </p>
             </div>
             <div class="col-md-4">
                 <label class="col-md-3 control-label">邮箱</label>
-                <p class="form-control-static">{{ $user['email'] }}</p>
+                <p class="form-control-static">{{ $user['email'] }}
+                    <button type="button" class="btn btn-outline btn-primary btn-xs" onclick="editEmail();">
+                        @if (empty($user['email']))
+                            绑定
+                        @else
+                            修改
+                        @endif
+                    </button>
+                </p>
             </div>
             <div class="col-md-4">
                 <label class="col-md-3 control-label">手机</label>
@@ -35,6 +45,20 @@
                 </p>
             </div>
         </div>
+    </form>
+    </div>
+    <!-- /.panel-body -->
+</div>
+<!-- /.panel -->
+<div class="panel panel-default">
+    <div class="panel-heading">详细信息
+        <a href="/home/user/edit">编辑</a>
+        <div class="pull-right">
+            <i class="fa fa-user"></i>个人信息
+        </div>
+    </div>
+    <div class="panel-body">
+    <form class="form-horizontal" role="form" method="POST" action="">
         @foreach ($user['details'] as $v)
             @if (@$i % 3 == 0)
                 <div class="form-group">
@@ -47,16 +71,50 @@
                 </div>
             @endif
         @endforeach
-    </form>
+        </form>
+        </div>
     </div>
-    <!-- /.panel-body -->
 </div>
-<!-- /.panel -->
 </div>
 </div>
 <script>
+function editUsername() {
+    $('#edit_username').modal('show');
+}
+function editEmail() {
+    $('#edit_email').modal('show');
+}
 function bindPhone() {
     $('#bind_phone').modal('show');
+}
+function confirmEditUsername() {
+    var username = $('input[name="username"]').val();
+    if (username.length == 0) {
+        showFailTip('请输入新用户名');
+        return false;
+    }
+    $.ajax({
+        url: '/api/user/edit',
+        data: {'username': username, 'access_token': 'test'},
+        dataType: 'json',
+        headers: {
+            'X-CSRF-TOKEN': $('input[name="_token"]').val()
+        },
+        success: function(data) {
+            if(data['code'] === 1) {
+                showSuccessTip(data['message']);
+                window.location.reload();
+            } else {
+                showFailTip(data['message']);
+                return false;
+            }
+        },
+        error: function(data) {
+            showFailTip(data['message']);
+            return false;
+        },
+    });
+
 }
 function sendCode() {
     var phone = $('input[name="phone"]').val();
@@ -142,6 +200,70 @@ function validateCode() {
 }
 </script>
 <!-- Modal -->
+<div class="modal fade" id="edit_username" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="width:400px; margin-top:40px;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title" id="myModalLabel">修改用户名</h5>
+            </div>
+            <div class="modal-body">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" name="username" placeholder="新用户名">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <button type="button" class="btn btn-primary btn-block" onClick="return confirmEditUsername();">确认</button>
+                    </div>
+                </div>
+            </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="edit_email" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="width:400px; margin-top:40px;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title" id="myModalLabel">修改邮箱</h5>
+            </div>
+            <div class="modal-body">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <input type="text" class="form-control" name="username" placeholder="新邮箱">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-md-12">
+                        <button type="button" class="btn btn-primary btn-block" onClick="return confirmEditEmail();">确认</button>
+                    </div>
+                </div>
+            </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- Modal -->
 <div class="modal fade" id="bind_phone" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog" style="width:400px; margin-top:40px;">
         <div class="modal-content">
@@ -180,5 +302,4 @@ function validateCode() {
     <!-- /.modal-dialog -->
 </div>
 <!-- /.modal -->
-</script>
 @endsection
