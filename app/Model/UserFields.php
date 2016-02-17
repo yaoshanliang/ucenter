ff5f98ce4a779628cd81de583c6d5b51c8d4edf8
yaoshanliang <1329517386@qq.com>
2016-02-17 09:48:17 +0800

    add validation

--- app/Model/UserFields.php
+++ app/Model/UserFields.php
@@ -6,5 +6,5 @@ use App\Model\Model;
 class UserFields extends Model
 {
 	protected $table = 'user_fields';
-    protected $fillable = ['name', 'title', 'type', 'description'];
+    protected $fillable = ['name', 'title', 'type', 'validation', 'description'];
 }
