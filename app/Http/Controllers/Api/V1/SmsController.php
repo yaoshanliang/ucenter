8d533e817a4b0ae7c23700fac65c8581b9c876f9
yaoshanliang <1329517386@qq.com>
2016-02-18 14:56:20 +0800

    add validate method

--- app/Http/Controllers/Api/V1/SmsController.php
+++ app/Http/Controllers/Api/V1/SmsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 
 use Cache;
 use Queue;
+use Config;
 use App\Services\Api;
 use App\Model\User;
 use Dingo\Api\Routing\Helpers;
@@ -29,15 +30,33 @@ class SmsController extends Controller
     // 发送验证码
     public function sendCode(Request $request)
     {
-        if ( 11 != strlen($request->get('phone'))) {
+        if ( 11 != strlen($phone = $request->get('phone'))) {
             return $this->response->array(array('code' => 0, 'message' => '手机号不合法'));
         }
 
-        Sms::enable('Luosimao', '80 backup');
-        // dd(PhpSms::queue());
-        // PhpSms::voice(1234)->to('18896581232')->send();exit;
-        PhpSms::make()->to('18896581232')->content('【签名】这是短信内容...')->send();
+        $code = rand(100000, 999999);
+        Cache::put(Config::get('cache.sms.code') . $phone, $code, 5);
+        // PhpSms::queue(false);
+        // PhpSms::make()->to('18896581232')->content('【签名】这是短信内容...')->send();
         // PhpSms::make()->to('18896581232')->template('Luosimao', 'xxx')->data(['12345', 5])->send();
-        return $this->response->array(compact('code', 'message', 'data'));
+        return $this->response->array(array('code' => 1, 'message' => '发送成功'));
+    }
+
+    // 验证验证码
+    public function validateCode(Request $request)
+    {
+        if ( 11 != strlen($phone = $request->get('phone'))) {
+            return $this->response->array(array('code' => 0, 'message' => '手机号不合法'));
+        }
+        if ( 0 == strlen($code = $request->get('code'))) {
+            return $this->response->array(array('code' => 0, 'message' => '验证码不合法'));
+        }
+
+        if (Cache::get(Config::get('cache.sms.code') . $phone) != $code) {
+            return $this->response->array(array('code' => 0, 'message' => '验证失败，验证码不匹配'));
+        } else {
+            Cache::put(Config::get('cache.sms.validated') . $phone, 1, 1);
+            return $this->response->array(array('code' => 1, 'message' => '验证成功'));
+        }
     }
 }
