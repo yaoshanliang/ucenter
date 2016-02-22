14dae454c37ff87f225559517a4cd37c1a1bc256
yaoshanliang <1329517386@qq.com>
2016-02-22 17:22:37 +0800

    add wechat login css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,16 +21,20 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
+                        <div class="col-md-3">
+                        </div>
+
+                        <div class="col-md-6">
                         <div class="form-group">
-                            <label class="col-md-4 control-label">账户</label>
-                            <div class="col-md-5">
+                            <label class="col-md-2 control-label">账户</label>
+                            <div class="col-md-9">
                                 <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="用户名/邮箱/手机">
                             </div>
                         </div>
 
                         <div class="form-group">
-                            <label class="col-md-4 control-label">密码</label>
-                            <div class="col-md-5">
+                            <label class="col-md-2 control-label">密码</label>
+                            <div class="col-md-9">
                                 <input type="password" class="form-control" name="password">
                             </div>
                         </div>
@@ -38,15 +42,15 @@
                         @if ($loginFailed)
                         <script src="//captcha.luosimao.com/static/js/api.js"></script>
                         <div class="form-group">
-                            <label class="col-md-4 control-label">验证</label>
-                            <div class="col-md-5">
+                            <label class="col-md-2 control-label">验证</label>
+                            <div class="col-md-9">
                                 <div class="l-captcha" data-site-key="{{ env('CAPTCHA_SITE_KEY') }}" data-width="100%"></div>
                             </div>
                         </div>
                         @endif
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-offset-2">
                                 <div class="checkbox">
                                     <label>
                                         <input type="checkbox" name="remember"> 记住我
@@ -56,12 +60,30 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-offset-2">
                                 <button type="submit" class="btn btn-primary">登录</button>
                                 <a class="btn btn-link" href="{{ url('/password/email') }}">忘记密码</a>
                                 <a class="btn btn-link" href="{{ url('/auth/register') }}">注册账户</a>
                             </div>
                         </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <script src="http://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
+                            <div id="login_container"></div>
+                            <script>
+                            var obj = new WxLogin({
+                                id: "login_container",
+                                appid: "<?php echo env('AppID'); ?>",
+                                scope: "snsapi_login",
+                                redirect_uri: "/auth/wechatCallback",
+                                state: "",
+                                style: "",
+                                href: "/css/wechat.css"
+                            });
+                            </script>
+                        </div>
+
                     </form>
                 </div>
             </div>
