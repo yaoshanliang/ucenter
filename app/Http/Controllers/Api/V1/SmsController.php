3f474dfb9fe0418b7f645d929578a0722fbd8393
yaoshanliang <1329517386@qq.com>
2016-02-24 21:31:28 +0800

    add send content

--- app/Http/Controllers/Api/V1/SmsController.php
+++ app/Http/Controllers/Api/V1/SmsController.php
@@ -30,21 +30,39 @@ class SmsController extends Controller
     // 发送验证码
     public function sendCode(Request $request)
     {
-        if ( 11 != strlen($phone = $request->get('phone'))) {
-            return $this->response->array(array('code' => 0, 'message' => '手机号不合法'));
+        $beforeSend = $this->beforeSend($request);
+        if (1 !== $beforeSend['code']) {
+            return $this->response->array(array('code' => 0, 'message' => $beforeSend['message']));
         }
 
         $code = rand(100000, 999999);
-        Cache::put(Config::get('cache.sms.code') . $phone, $code, 5);
+        Cache::put(Config::get('cache.sms.code') . $request->phone, $code, 5);
         PhpSms::queue(false);
-        $result = PhpSms::make()->to('18896581232')->content('您好，您的验证码是：' . $code)->send();
+        $result = PhpSms::make()->to($request->phone)->content('您好，您的验证码是：' . $code)->send();
 
         if ( true === $result['success']) {
             return $this->response->array(array('code' => 1, 'message' => '发送成功'));
         } else {
             return $this->response->array(array('code' => 0, 'message' => '发送失败', 'data' => $result['logs']));
         }
+    }
 
+    // 发送内容短信
+    public function sendContent(Request $request)
+    {
+        $beforeSend = $this->beforeSend($request);
+        if (1 !== $beforeSend['code']) {
+            return $this->response->array(array('code' => 0, 'message' => $beforeSend['message']));
+        }
+
+        PhpSms::queue(false);
+        $result = PhpSms::make()->to($request->phone)->content($request->content)->send();
+
+        if ( true === $result['success']) {
+            return $this->response->array(array('code' => 1, 'message' => '发送成功'));
+        } else {
+            return $this->response->array(array('code' => 0, 'message' => '发送失败', 'data' => $result['logs']));
+        }
     }
 
     // 验证验证码
@@ -63,5 +81,25 @@ class SmsController extends Controller
             Cache::put(Config::get('cache.sms.validated') . $phone, 1, 1);
             return $this->response->array(array('code' => 1, 'message' => '验证成功'));
         }
+    }
+
+    // 发送前的校验
+    private function beforeSend(Request $request)
+    {
+        if ( 11 != strlen($request->get('phone'))) {
+            return array('code' => 0, 'message' => '手机号不合法');
+        }
+
+        $count = Cache::get(Config::get('cache.sms.count') . self::$currentUserId);
+        if (is_null($count)) {
+            Cache::put(Config::get('cache.sms.count') . self::$currentUserId, 1, 60);
+        } else {
+            if (!in_array($request->phone, Config::get('phpsms.whiteList')) && $count > 3) {
+                return array('code' => 0, 'message' => '超过每小时三次限制');
+            }
+            Cache::put(Config::get('cache.sms.count') . self::$currentUserId, ++$count, 60);
+        }
+
+        return array('code' => 1, 'message' => '');
     }
 }
