3ee3894a7108cf0022a91f6ef8c3be0fb1ae71a6
yaoshanliang <1329517386@qq.com>
2016-02-25 17:21:43 +0800

    add get auth code

--- app/Http/Controllers/Api/V1/OauthController.php
+++ app/Http/Controllers/Api/V1/OauthController.php
@@ -8,8 +8,26 @@ use Dingo\Api\Routing\Helpers;
 class OauthController extends Controller
 {
     use Helpers;
+
+    // 获取access_token
     public function getAccessToken()
     {
         return $this->response->array(array('code' => 1, 'message' => '获取access_token成功', 'data' => Authorizer::issueAccessToken()));
+    }
+
+    // 获取授权码
+    public function getAuthCode()
+    {
+        $authParams = Authorizer::getAuthCodeRequestParams();
+        $formParams = array_except($authParams,'client');
+        $formParams['client_id'] = $authParams['client']->getId();
+        $formParams['scope'] = implode(config('oauth2.scope_delimiter'), array_map(function ($scope) {
+            return $scope->getId();
+        }, $authParams['scopes']));
+        $params['user_id'] = 1000;
+
+        $redirectUri = Authorizer::issueAuthCode('user', $params['user_id'], $params);
+
+        return substr(strstr($redirectUri, '?code'), 6);
     }
 }
