7910dff04354768a4fb0c3b566e44b0cb6a8d7b1
yaoshanliang <1329517386@qq.com>
2016-02-28 21:17:21 +0800

    update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,69 @@
-@extends('app')
+@extends('auth.base')
 
 @section('content')
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">注册</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">注册</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">用户名</label>
-							<div class="col-md-6">
-								<input type="text" class="form-control" name="username" value="{{ old('username') }}">
-							</div>
-						</div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">手机号</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="phone">
+                            </div>
+                        </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">邮箱</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">验证码</label>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="code">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="button" id="send_code" class="btn btn-outline btn-success" onClick="return sendCode();" value="发送验证码">
+                            </div>
+                        </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">密码</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">密码</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                        </div>
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">确认密码</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password_confirmation">
-							</div>
-						</div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">确认密码</label>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password_confirmation">
+                            </div>
+                        </div>
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">
-									注册
-								</button>
-								<a class="btn btn-link" href="{{ url('/auth/login') }}">返回登录</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    注册
+                                </button>
+                                <a class="btn btn-link" href="{{ url('/auth/login') }}">返回登录</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
