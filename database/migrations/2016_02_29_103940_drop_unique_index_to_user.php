8e3447dd1d302c6c4b7d193c028326860c1c4d70
yaoshanliang <1329517386@qq.com>
2016-02-29 10:43:06 +0800

    drop unique index of email

---
+++ database/migrations/2016_02_29_103940_drop_unique_index_to_user.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropUniqueIndexToUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('users_email_unique');
+            $table->index('email');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
