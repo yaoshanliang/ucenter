70da8f5705a8cc2d5970ef7df84a64585e96d279
yaoshanliang <1329517386@qq.com>
2016-02-29 16:27:47 +0800

    update authorize

--- resources/views/oauth/authorize.blade.php
+++ resources/views/oauth/authorize.blade.php
@@ -19,24 +19,27 @@
 					@endif
 
                     <form method="post" action="{{ route('oauth.authorize.post', $params) }}">
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-                                <h2>{{ $client->getName() }}</h2>
-                                将访问您的如下资料：
-                                <input type="hidden" name="client_id" value="{{$params['client_id']}}">
-                                <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
-                                <input type="hidden" name="response_type" value="{{$params['response_type']}}">
-                                <input type="hidden" name="state" value="{{$params['state']}}">
-                                <input type="hidden" name="scope" value="{{$params['scope']}}">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-							</div>
-						</div>
+                        <input type="hidden" name="client_id" value="{{$params['client_id']}}">
+                        <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
+                        <input type="hidden" name="response_type" value="{{$params['response_type']}}">
+                        <input type="hidden" name="state" value="{{$params['state']}}">
+                        <input type="hidden" name="scope" value="{{$params['scope']}}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-5">
+                            <div class="form-group">
+                                <p class="lead">{{ $client->getName() }}</p>
+                                将访问您的如下资料：
+                                <ul>
+                                <li class="text-success">个人信息</li>
+                                </ul>
+                            </div>
+                            <div class="form-group">
 								<button type="submit" name="approve" value="1" class="btn btn-primary">授权</button>
 								<button type="submit" name="deny" value="1" class="btn btn-primary">取消</button>
-							</div>
+                            </div>
 						</div>
 					</form>
 				</div>
