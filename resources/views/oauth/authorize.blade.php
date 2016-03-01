1f548878cbf1f14419bd0c2855b6da373c8633fb
yaoshanliang <1329517386@qq.com>
2016-03-01 08:58:24 +0800

    update authorize view

--- resources/views/oauth/authorize.blade.php
+++ resources/views/oauth/authorize.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">授权</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">授权</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
                     <form method="post" action="{{ route('oauth.authorize.post', $params) }}">
                         <input type="hidden" name="client_id" value="{{$params['client_id']}}">
@@ -37,14 +37,14 @@
                                 </ul>
                             </div>
                             <div class="form-group">
-								<button type="submit" name="approve" value="1" class="btn btn-primary">授权</button>
-								<button type="submit" name="deny" value="1" class="btn btn-primary">取消</button>
+                                <button type="submit" name="approve" value="1" class="btn btn-primary">授权</button>
+                                <button type="submit" name="deny" value="1" class="btn btn-primary">取消</button>
                             </div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
