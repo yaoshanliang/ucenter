f7ef73e0f87a0dc740ca167f09b7ef964b2e3c96
yaoshanliang <1329517386@qq.com>
2016-03-02 13:18:32 +0800

    add get permissions

--- app/Http/Controllers/Api/V1/RoleController.php
+++ app/Http/Controllers/Api/V1/RoleController.php
@@ -14,16 +14,39 @@ class RoleController extends ApiController
 {
     use Helpers;
 
+    // 获取当前应用当前用户的角色
     public function getUserRole(Request $request)
     {
-        $roleIdsArray = UserRole::where('app_id', parent::$currentAppId)
-            ->where('user_id', parent::$currentUserId)->lists('role_id');
-        if (empty($roleIdsArray)) {
-            return $this->response->array(array('code' => 1, 'message' => '验证成功'));
+        $roles = Cache::get(Config::get('cache.user_role.app_id') . parent::$currentAppId . ':user_id:' . parent::$currentUserId);
+
+        if (empty($roles['roles'])) {
+            return $this->response->array(array('code' => 0, 'message' => '当前用户没有角色'));
         }
-        foreach ($roleIdsArray as $v) {
-            $roles[] = Cache::get(Config::get('cache.roles') . $v);
-        }
+
         return $this->response->array(array('code' => 1, 'message' => '获取角色成功', 'data' => $roles));
+    }
+
+    // 获取当前应用当前用户的权限
+    public function getUserPermission(Request $request)
+    {
+        $roles = Cache::get(Config::get('cache.user_role.app_id') . parent::$currentAppId . ':user_id:' . parent::$currentUserId);
+        if (empty($roles['roles'])) {
+            return $this->response->array(array('code' => 0, 'message' => '当前用户没有权限'));
+        }
+        $data['user_id'] = $roles['user_id'];
+        $permissions = array();
+        foreach ($roles['roles'] as $value) {
+            foreach ($value['permissions'] as $v) {
+                $permissions[$v['id']] = $v;
+            }
+        }
+
+        if (empty($permissions)) {
+            return $this->response->array(array('code' => 0, 'message' => '当前用户没有权限'));
+        }
+
+        $data['permissions'] = array_values($permissions);
+
+        return $this->response->array(array('code' => 1, 'message' => '获取权限成功', 'data' => $data));
     }
 }
