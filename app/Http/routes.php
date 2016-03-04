87f31359e9e4be5e2be50d7c65891eed6c43309f
yaoshanliang <1329517386@qq.com>
2016-03-04 09:49:59 +0800

    update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,114 +19,134 @@ Route::group(['middleware' => 'csrf'], function() {
     ]);
 });
 
-Route::group(['prefix' => 'home', 'namespace' => 'Home', 'middleware' => 'auth', 'csrf'], function() {
-    Route::get('/', 'HomeController@index');
-    Route::get('/index', 'HomeController@index');
-
-    // user
-    Route::group(['prefix' => 'user'], function(){
-        Route::get('/edit', 'UserController@edit');
-        Route::get('/wechatCallback', 'UserController@wechatCallback');
-        // Route::post('/edit', 'UserController@postEdit');
+Route::group(['prefix' => 'home', 'namespace' => 'Home', 'middleware' => ['auth']], function() {
+    Route::group([], function () {
+        Route::put('/app/currentApp', 'AppController@putCurrentApp');
+        Route::put('/app/currentRole', 'AppController@putCurrentRole');
     });
-    Route::resource('/user', 'UserController');
 
-    // app
-    Route::group(['prefix' => 'app'], function(){
-        Route::post('/lists', 'AppController@lists');
-        Route::post('/remove', 'AppController@remove');
-        Route::post('/setCurrentApp', 'AppController@setCurrentApp');
-        Route::post('/setCurrentRole', 'AppController@setCurrentRole');
+    Route::group(['middleware' => ['csrf']], function () {
+        Route::get('/', 'HomeController@index');
+        Route::get('/index', 'HomeController@index');
+
+        // user
+        Route::group(['prefix' => 'user'], function(){
+            Route::get('/edit', 'UserController@edit');
+            Route::get('/wechatCallback', 'UserController@wechatCallback');
+            // Route::post('/edit', 'UserController@postEdit');
+        });
+        Route::resource('/user', 'UserController');
+
+        // app
+        Route::group(['prefix' => 'app'], function(){
+        });
+        Route::resource('/app', 'AppController');
     });
-    Route::resource('/app', 'AppController');
 });
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => array('auth', 'csrf', 'role:developer|admin')], function() {
-    Route::get('/', function() { return Redirect::to('/admin/index'); });
-    Route::get('/forbidden', 'AdminController@forbidden');
-    Route::get('/index', 'AdminController@index');
-
-    // app，require developer role
-    Route::group(['prefix' => 'app', 'middleware' => 'role:developer'], function() {
-        Route::post('/lists', 'AppController@lists');
-        Route::post('/delete', 'AppController@delete');
-        Route::post('/setCurrentApp', 'AppController@setCurrentApp');
-        Route::post('/setCurrentRole', 'AppController@setCurrentRole');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => array('auth', 'role:developer|admin')], function() {
+    Route::group([], function () {
+        Route::put('/app/currentApp', 'AppController@putCurrentApp');
+        Route::put('/app/currentRole', 'AppController@putCurrentRole');
     });
-    Route::resource('/app', 'AppController');
+    Route::group(['middleware' => 'csrf'], function () {
+        Route::get('/', function() { return Redirect::to('/admin/index'); });
+        Route::get('/index', 'AdminController@index');
 
-    // user
-    Route::group(['prefix' => 'user'], function(){
-        Route::get('/invite', 'UserController@getInvite');
-        Route::post('/invite', 'UserController@postInvite');
-        Route::post('/lists', 'UserController@lists');
-        Route::post('/delete', 'UserController@delete');
-        Route::post('/remove', 'UserController@remove');
-        Route::get('/all', 'UserController@all');
-        Route::post('/allLists', 'UserController@allLists');
-        Route::get('/fields', 'UserController@fields');
-        Route::post('/fieldsLists', 'UserController@fieldsLists');
-        Route::get('/{id}/roles', 'UserController@roles');
-        Route::get('/{id}/selectOrUnselectRole/{role_id}', 'UserController@selectOrUnselectRole')->where('id', '[0-9]+')->where('role_id', '[0-9]+');
-    });
-    Route::resource('/user', 'UserController');
+        // app，require developer role
+        Route::group(['prefix' => 'app', 'middleware' => 'role:developer'], function() {
+            // Route::post('/currentApp', 'AppController@currentApp');
+        });
+        Route::resource('app', 'AppController');
+        // Route::controller('app', 'AppController');
 
-    // role
-    Route::group(['prefix' => 'role'], function(){
-        Route::post('/lists', 'RoleController@lists');
-        Route::post('/delete', 'RoleController@delete');
-        Route::get('/{id}/permission', 'RoleController@permission')->where('id', '[0-9]+');
-        Route::post('/{id}/permissionLists', 'RoleController@permissionLists')->where('id', '[0-9]+');
-        Route::get('/{id}/permissionSelected', 'RoleController@permissionSelected')->where('id', '[0-9]+');
-        Route::post('/{id}/permissionSelectedLists', 'RoleController@permissionSelectedLists');
-        Route::get('/{id}/permissionGroup/{permission_id}', 'RoleController@permissionGroup')->where('id', '[0-9]+')->where('permission_id', '[0-9]+');
-        Route::get('/{id}/selectOrUnselectPermission/{permission_id}', 'RoleController@selectOrUnselectPermission')->where('id', '[0-9]+')->where('permission_id', '[0-9]+');
-    });
-    Route::resource('/role', 'RoleController');
+        // user
+        Route::group(['prefix' => 'user'], function(){
+            Route::get('/invite', 'UserController@getInvite');
+            Route::post('/invite', 'UserController@postInvite');
+            Route::post('/lists', 'UserController@lists');
+            Route::post('/delete', 'UserController@delete');
+            Route::post('/remove', 'UserController@remove');
+            Route::get('/all', 'UserController@all');
+            Route::post('/allLists', 'UserController@allLists');
+            Route::get('/fields', 'UserController@fields');
+            Route::post('/fieldsLists', 'UserController@fieldsLists');
+            Route::get('/{id}/roles', 'UserController@roles');
+            Route::get('/{id}/selectOrUnselectRole/{role_id}', 'UserController@selectOrUnselectRole')->where('id', '[0-9]+')->where('role_id', '[0-9]+');
+        });
+        Route::resource('/user', 'UserController');
 
-    // permission
-    Route::group(['prefix' => 'permission'], function(){
-        Route::post('/lists', 'PermissionController@lists');
-        Route::post('/delete', 'PermissionController@delete');
-        Route::get('/group', 'PermissionController@group');
-        Route::post('/groupLists', 'PermissionController@groupLists');
-        Route::get('/createGroup', 'PermissionController@createGroup');
-    });
-    Route::resource('/permission', 'PermissionController');
+        // role
+        Route::group(['prefix' => 'role'], function(){
+            Route::post('/lists', 'RoleController@lists');
+            Route::post('/delete', 'RoleController@delete');
+            Route::get('/{id}/permission', 'RoleController@permission')->where('id', '[0-9]+');
+            Route::post('/{id}/permissionLists', 'RoleController@permissionLists')->where('id', '[0-9]+');
+            Route::get('/{id}/permissionSelected', 'RoleController@permissionSelected')->where('id', '[0-9]+');
+            Route::post('/{id}/permissionSelectedLists', 'RoleController@permissionSelectedLists');
+            Route::get('/{id}/permissionGroup/{permission_id}', 'RoleController@permissionGroup')->where('id', '[0-9]+')->where('permission_id', '[0-9]+');
+            Route::get('/{id}/selectOrUnselectPermission/{permission_id}', 'RoleController@selectOrUnselectPermission')->where('id', '[0-9]+')->where('permission_id', '[0-9]+');
+        });
+        Route::resource('/role', 'RoleController');
 
-    // file
-    Route::group(['prefix' => 'file'], function(){
-    });
-    Route::resource('/file', 'FileController');
+        // permission
+        Route::group(['prefix' => 'permission'], function(){
+            Route::post('/lists', 'PermissionController@lists');
+            Route::post('/delete', 'PermissionController@delete');
+            Route::get('/group', 'PermissionController@group');
+            Route::post('/groupLists', 'PermissionController@groupLists');
+            Route::get('/createGroup', 'PermissionController@createGroup');
+        });
+        Route::resource('/permission', 'PermissionController');
 
-    // mail
-    Route::group(['prefix' => 'mail'], function(){
-    });
-    Route::resource('/mail', 'MailController');
+        // file
+        Route::group(['prefix' => 'file'], function(){
+        });
+        Route::resource('/file', 'FileController');
 
-    // message
-    Route::group(['prefix' => 'message'], function(){
-    });
-    Route::resource('/message', 'MessageController');
+        // mail
+        Route::group(['prefix' => 'mail'], function(){
+        });
+        Route::resource('/mail', 'MailController');
 
-    // userlog
-    Route::group(['prefix' => 'userlog'], function(){
-        Route::post('/lists', 'UserLogController@lists');
-        Route::post('/delete', 'UserLogController@delete');
+        // message
+        Route::group(['prefix' => 'message'], function(){
+        });
+        Route::resource('/message', 'MessageController');
+
+        // userlog
+        Route::group(['prefix' => 'userlog'], function(){
+            Route::post('/lists', 'UserLogController@lists');
+            Route::post('/delete', 'UserLogController@delete');
+        });
+        Route::resource('/userlog', 'UserLogController');
+
     });
-    Route::resource('/userlog', 'UserLogController');
 });
 
 $api = app('api.router');
-$api->version('v1', ['middleware' => ['oauth']], function ($api) {
-    $api->get('user/getUserInfo', 'App\Http\Controllers\Api\V1\UserController@getUserInfo');
-    $api->get('user/edit', 'App\Http\Controllers\Api\V1\UserController@edit');
-    $api->get('sms/sendCode', ['middleware' => ['oauth'], 'uses' => 'App\Http\Controllers\Api\V1\SmsController@sendCode']);
-    $api->get('sms/validateCode', 'App\Http\Controllers\Api\V1\SmsController@validateCode');
+$api->version('v1', ['namespace' => 'App\Http\Controllers\Api\V1'], function ($api) {
+    $api->group(['middleware' => 'oauth'], function ($api) {
+        $api->get('user/info', 'UserController@getUserInfo');
+        $api->get('user/role', 'UserController@getUserRole');
+        $api->get('user/permission', 'UserController@getUserPermission');
+        $api->put('user/info', 'UserController@edit');
+
+        $api->put('app/secret', 'AppController@updateSecret');
+        $api->post('sms/code', ['middleware' => ['oauth'], 'uses' => 'SmsController@sendCode']);
+        $api->put('sms/code', 'SmsController@validateCode');
+    });
+    $api->group([], function ($api) {
+        $api->post('oauth/accessToken', 'OauthController@getAccessToken');
+        $api->get('oauth/authCode', ['middleware' => ['check-authorization-params'], 'uses' => 'OauthController@getAuthCode']);
+
+        $api->put('app/currentApp', 'AppController@updateCurrentApp');
+        $api->put('app/currentRole', 'AppController@updateCurrentRole');
+    });
 });
 
-Route::post('api/oauth/getAccessToken', 'Api\V1\OauthController@getAccessToken');
-Route::get('api/oauth/getAuthCode', ['middleware' => ['check-authorization-params'], 'uses' => 'Api\V1\OauthController@getAuthCode']);
+// Route::post('api/oauth/accessToken', 'Api\V1\OauthController@getAccessToken');
+// Route::get('api/oauth/authCode', ['middleware' => ['check-authorization-params'], 'uses' => 'Api\V1\OauthController@getAuthCode']);
 
 Route::get('/oauth/authorize', ['as' => 'oauth.authorize.get', 'middleware' => ['check-authorization-params', 'auth'], 'uses' => 'Oauth\OauthController@getAuthorize']);
 Route::post('/oauth/authorize', ['as' => 'oauth.authorize.post', 'middleware' => ['csrf', 'check-authorization-params', 'auth'], 'uses' => 'Oauth\OauthController@postAuthorize']);
