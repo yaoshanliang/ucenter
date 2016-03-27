cd38146a968d7bb4bcc825446c9f1deaa6bd3107
yaoshanliang <1329517386@qq.com>
2016-03-27 18:23:13 +0800

    update api service

--- app/Services/Api.php
+++ app/Services/Api.php
@@ -1,48 +1,34 @@
-<?php namespace App\Services;
+<?php
+
+namespace App\Services;
+
 use Illuminate\Support\Facades\Lang;
+
 class Api
 {
-	/**
-	 * 返回jsonp格式数据
-	 *
-	 * @param  string $status_code, string $status_msg, array $status_data
-	 * @return jsonp格式字符串
-	 */
-	public static function jsonpReturn($status_code, $status_msg, $status_data = array()) {
-		$data['status_code'] = $status_code;
-		$data['status_msg'] = $status_msg;
-		$data['status_data'] = $status_data;
-		$jsonp = preg_match('/^[$A-Z_][0-9A-Z_$]*$/i', $_GET['callback']) ? $_GET['callback'] : false;
-		if($jsonp) {
-		    echo $jsonp . '(' . json_encode($data, JSON_UNESCAPED_UNICODE) . ');';
-		}
-	}
+    /**
+     * apiReturn
+     *
+     * @param int $code 代码
+     * @param string $message 消息
+     * @param array $data 数据
+     *
+     * @return json
+     */
+    public static function apiReturn($code, $message = '', $data = [])
+    {
+        $data = (object)$data;
 
-	/**
-	 * 返回json格式数据
-	 *
-	 * @param  int $code, string $msg, array $data
-	 * @return json格式字符串
-	 */
-	public static function jsonReturn($code, $message = '', $data = null) {
-		if(empty($message)) {
-			$message = Lang::has('api.' . $code) ? trans('api.' . $code) : '';
-		}
-		if(is_null($data)) {
-			return response()->json(array('code' => $code, 'message' => $message));
-			header('Content-Type: application/json');
-			return json_encode(array('code' => $code, 'message' => $message));
-		} else {
-			return response()->json(array('code' => $code, 'message' => $message, 'data' => $data));
-			header('Content-Type: application/json');
-			return json_encode(array('code' => $code, 'message' => $message, 'data' => $data));
-		}
-	}
-
-    public static function apiReturn() {
         return response()->json(compact('code', 'message', 'data'));
     }
 
+    /**
+     * dataTablesReturn
+     *
+     * @param array $data 数据
+     *
+     * @return json
+     */
     public static function dataTablesReturn($data)
     {
         return response()->json($data);
