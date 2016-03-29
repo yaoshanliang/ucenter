8e3a12188399fb1735c5643ef5b15f76d1df096c
yaoshanliang <1329517386@qq.com>
2016-03-29 11:04:57 +0800

    update sms api

--- app/Http/Controllers/Api/V1/SmsController.php
+++ app/Http/Controllers/Api/V1/SmsController.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Http\Controllers\Api\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Api\V1\ApiController;
 
 use Cache;
 use Queue;
@@ -13,27 +14,24 @@ use App\Model\User;
 use Dingo\Api\Routing\Helpers;
 use LucaDegasperi\OAuth2Server\Facades\Authorizer;
 use PhpSms;
-use Toplan\PhpSms\Sms;
+// use Toplan\PhpSms\Sms;
 
-class SmsController extends Controller
+class SmsController extends ApiController
 {
     // 发送验证码
-    public function sendCode(Request $request)
+    public function postCode(Request $request)
     {
-        $beforeSend = $this->beforeSend($request);
-        if (1 !== $beforeSend['code']) {
-            return $this->response->array(array('code' => 0, 'message' => $beforeSend['message']));
-        }
+        $this->beforeSend($request);
 
         $code = rand(100000, 999999);
         Cache::put(Config::get('cache.sms.code') . $request->phone, $code, 5);
         PhpSms::queue(false);
         $result = PhpSms::make()->to($request->phone)->content('您好，您的验证码是：' . $code)->send();
 
-        if ( true === $result['success']) {
-            return $this->response->array(array('code' => 1, 'message' => '发送成功'));
+        if (true === $result['success']) {
+            return Api::apiReturn(SUCCESS, '发送成功');
         } else {
-            return $this->response->array(array('code' => 0, 'message' => '发送失败', 'data' => $result['logs']));
+            return Api::apiReturn(ERROR, '发送失败', $result['logs']);
         }
     }
 
@@ -48,37 +46,30 @@ class SmsController extends Controller
         PhpSms::queue(false);
         $result = PhpSms::make()->to($request->phone)->content($request->content)->send();
 
-        if ( true === $result['success']) {
-            return $this->response->array(array('code' => 1, 'message' => '发送成功'));
+        if (true === $result['success']) {
+            return Api::apiReturn(SUCCESS, '发送成功');
         } else {
-            return $this->response->array(array('code' => 0, 'message' => '发送失败', 'data' => $result['logs']));
+            return Api::apiReturn(ERROR, '发送失败', $result['logs']);
         }
     }
 
     // 验证验证码
     public function validateCode(Request $request)
     {
-        if ( 11 != strlen($phone = $request->get('phone'))) {
-            return $this->response->array(array('code' => 0, 'message' => '手机号不合法'));
-        }
-        if ( 0 == strlen($code = $request->get('code'))) {
-            return $this->response->array(array('code' => 0, 'message' => '验证码不合法'));
-        }
+        $this->apiValidate($request->only('phone', 'code'), ['phone' => 'required|size:11', 'code' => 'required|size:6']);
 
-        if (Cache::get(Config::get('cache.sms.code') . $phone) != $code) {
-            return $this->response->array(array('code' => 0, 'message' => '验证失败，验证码不匹配'));
+        if (Cache::get(Config::get('cache.sms.code') . $request->phone) != $request->code) {
+            return Api::apiReturn(ERROR, '验证失败，验证码不匹配');
         } else {
-            Cache::put(Config::get('cache.sms.validated') . $phone, 1, 1);
-            return $this->response->array(array('code' => 1, 'message' => '验证成功'));
+            Cache::put(Config::get('cache.sms.validated') . $request->phone, 1, 1);
+            return Api::apiReturn(SUCCESS, '验证成功');
         }
     }
 
     // 发送前的校验
     private function beforeSend(Request $request)
     {
-        if ( 11 != strlen($request->get('phone'))) {
-            return array('code' => 0, 'message' => '手机号不合法');
-        }
+        $this->apiValidate($request->only('phone'), ['phone' => 'required|size:11']);
 
         // 手机号和用户id两种方式计数
         $count = empty(parent::getUserId()) ? Cache::get(Config::get('cache.sms.count.phone') . $request->phone) :
@@ -89,12 +80,12 @@ class SmsController extends Controller
                 Cache::put(Config::get('cache.sms.count.user_id') . parent::getUserId(), 1, 60);
         } else {
             if (!in_array($request->phone, Config::get('phpsms.whiteList')) && $count > 3) {
-                return array('code' => 0, 'message' => '超过每小时三次限制');
+                throw new ApiException('超过每小时三次限制');
             }
             empty(parent::getUserId()) ? Cache::put(Config::get('cache.sms.count.phone') . $request->phone, ++$count, 60) :
                 Cache::put(Config::get('cache.sms.count.user_id') . parent::getUserId(), ++$count, 60);
         }
 
-        return array('code' => 1, 'message' => '发送前校验成功');
+        return true;
     }
 }
