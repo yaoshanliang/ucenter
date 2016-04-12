65d89f75e71d85e271d9d80745e4c80538d1dbd8
yaoshanliang <1329517386@qq.com>
2016-04-12 09:24:53 +0800

    add file view

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -1,15 +1,55 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use Cache;
+use Config;
+use Session;
+use App\Model\File;
+
 class FileController extends Controller
 {
+    // 当前应用文件列表
     public function getIndex(Request $request)
     {
         return view('admin.file.index');
+    }
+
+    public function postLists(Request $request)
+    {
+        $fields = array('id', 'user_id', 'file_name', 'file_path', 'mime_type', 'size', 'created_at');
+        $searchFields = array('file_name', 'file_path');
+
+        $data = File::where('app_id', Session::get('current_app_id'))
+            ->whereDataTables($request, $searchFields)
+            ->orderByDataTables($request)
+            ->skip($request->start)
+            ->take($request->length)
+            ->get($fields);
+        foreach ($data as &$v) {
+            $user = Cache::get(Config::get('cache.users') . $v->user_id);
+            $v->user_id = $user['username'];
+        }
+        $draw = (int)$request->draw;
+        $recordsTotal = File::where('app_id', Session::get('current_app_id'))->count();
+        $recordsFiltered = strlen($request->search['value']) ? count($data) : $recordsTotal;
+
+        return $this->response->array(compact('draw', 'recordsFiltered', 'recordsTotal', 'data'));
+    }
+
+    // 显示邮件详细
+    public function getShow(Request $request, $id)
+    {
+        if (! Email::where('app_id', Session::get('current_app_id'))->exists()) {
+            return $this->response->array(array('code' => 0, 'message' => 'Forbidden'));
+        }
+
+        $email = Email::find($id);
+
+        return view('admin.email.show')->with(compact('email'));
     }
 }
--- resources/views/admin/file/index.blade.php
+++ resources/views/admin/file/index.blade.php
@@ -4,12 +4,75 @@
 <div class="row">
 <div class="col-lg-12">
 <div class="panel panel-default">
-    <div class="panel-heading">文件管理</div>
+    <div class="panel-heading">文件管理
+        <div class="pull-right">
+            <i class="fa fa-th"></i> 文 件
+        </div>
+    </div>
     <div class="panel-body">
+        <div class="dataTable_wrapper">
+            <br />
+            <div class="input-group custom-search-form">
+                <input type="text" id="search" class="form-control search" placeholder="搜索">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/file') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <table class="table table-striped table-bordered table-hover" id="file_index" class="display" cellspacing="0" width="100%" border='0px'>
+                    <thead>
+                        <tr>
+                            <td style="width:15px"><input class="checkbox" type="checkbox" name="id" id='checkAll'></td>
+                            <td>文件</td>
+                            <td>类型</td>
+                            <td>大小</td>
+                            <td>上传者</td>
+                            <td>上传时间</td>
+                            <td>操作</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+        <!-- /.dataTable_wrapper -->
     </div>
     <!-- /.panel-body -->
 </div>
 <!-- /.panel -->
 </div>
 </div>
+
+<script>
+var datatable_id = 'file_index';
+var columnDefs_targets = [0, 6];
+var order = [5, 'desc'];
+var ajax_url = '/admin/file/lists';
+var columns = [{
+                    "data": "id",
+                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                        $(nTd).html("<input type='checkbox' id='" + sData + "' class='checkbox' name='ids' value='" + sData + "'>");
+                    }
+                },
+                {
+                    "data": "file_name",
+                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                        $(nTd).html("<a target='_blank' href=" + oData['file_path'] + ">" + oData['file_name'] + "</a>");
+                    }
+                },
+                {"data": "mime_type"},
+                {"data": "size"},
+                {"data": "user_id"},
+                {"data": "created_at"},
+                {
+                    "data": "id",
+                    "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+                        $(nTd).html('');
+                    }
+                }];
+</script>
 @endsection
