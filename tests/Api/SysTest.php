cc5d19bb15a7c94dac1ce2c927a1ca179604b4fb
yaoshanliang <1329517386@qq.com>
2016-04-20 21:06:42 +0800

    update cache api

--- tests/Api/SysTest.php
+++ tests/Api/SysTest.php
@@ -6,6 +6,6 @@ class SysTest extends ApiTest
 {
     public function testGetAccessToken()
     {
-        $accessToken = $this->get('/api/sys/cache')->seeJson(['code' => 1]);
+        $cache = $this->get('/api/sys/cache')->seeJson(['code' => 1]);
     }
 }
