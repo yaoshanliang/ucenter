1c2463416b461b8548974592977f80de497e303a
yaoshanliang <1329517386@qq.com>
2016-04-23 09:26:58 +0800

    rm example

--- example/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('app', 'example');//应用名称，需要申请
-define('app_secret', 'example_secret');//应用密钥，需要申请
-define('site_home', 'http://example.iat.net.cn');//本站地址
-define('ucenter_home', 'http://ucenter.iat.net.cn');//用户中心地址
-?>
--- example/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('config.php');
-session_start();
-session_destroy();
-header('Location:' . site_home);
-?>
