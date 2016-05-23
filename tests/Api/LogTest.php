c6e77038275923a8ab4c04518eb8a3e4b8f7a471
yaoshanliang <1329517386@qq.com>
2016-05-23 09:07:29 +0800

    update to accessToken

--- tests/Api/LogTest.php
+++ tests/Api/LogTest.php
@@ -7,7 +7,7 @@ class LogTest extends ApiTest
     public function testPostLog()
     {
         $email = $this->post('/api/log', ['type' => 'A', 'title' => 'test',
-            'data' => 'test data', 'sql' => 'insert into', 'access_token' => 'test'])
+            'data' => 'test data', 'sql' => 'insert into', 'access_token' => $this->accessToken])
             ->seeJson(['code' => 1]);
     }
 }
