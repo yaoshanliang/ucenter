3a2257994f85146fda4d4451dc543f3a47677188
iat <i@iat.net.cn>
2017-06-11 14:41:19 +0800

    改版

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,131 +1,86 @@
-<!DOCTYPE HTML>
-<html>
-<head> 
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title></title>
-<link rel="stylesheet" type="text/css" href="{{ asset('/css/login.css') }}"> 
-<script src="{{ asset('/js/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/login.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/jquery.mousewheel.min.js') }}"  type="text/javascript"></script>
-<script type="text/javascript">
-</script>  
-</head> 
-<body>
-<div class="top section"></div>
-<!-- container -->
-<div id="formbackground" style="position:absolute; width:100%; height:100%; z-index:-1">  
-<img src="" height="100%" width="100%" class="backgroundimg"/>  
-</div>
-<section id="section1" class="section section1">
-  <article class="sectionWrapper section1Wrapper fadeInDown">
-      <div class="container">
-    <div class="login">
-        <div class="login-first">
-            <div class="login-font1">
-                <font style="margin-left:10px; color:#232323">
-                    <a class="navbar-brand" href="{{url('/')}}">{{ Cache::get('settings:site_name') }}</a>
-                    <a class="navbar-brand" href="{{url('/')}}">{{ Cache::get('settings:site_sub_name') }}</a>
-                </font>
-            </div>
-            <div class="login-font">统一身份认证</div>
-            @if (count($errors) > 0)
-                <div class="alert alert-danger danger-warning">
-                    <strong>Whoops!</strong> There were some problems with your input.
-                    <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                    </ul>
-                </div>
-            @endif 
-            <div class="login-input">
-                <form  role="form" method="POST" action="{{ url('/auth/login') }}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <div><font class="font">账号:</font>
-                        <input type="text" class="userName"  name="username" value="{{ old('username') }}" placeholder="用户名/邮箱/手机" style="margin-top:1.5%;"></div>
-                    <div><font class="font">密码:</font>
-                        <input type="password" class="password"  name="password" placeholder="密码"></div>
-                     @if ($loginFailed)
+@extends('auth.base')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">登录</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        <div class="col-md-3">
+                        </div>
+
+                        <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">账户</label>
+                            <div class="col-md-10">
+                                <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="用户名/邮箱/手机">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">密码</label>
+                            <div class="col-md-10">
+                                <input type="password" class="form-control" name="password" placeholder="密码">
+                            </div>
+                        </div>
+
+                        @if ($loginFailed)
                         <script src="//captcha.luosimao.com/static/js/api.js"></script>
-                        <div class="form-group verification">
-                            <label class="col-md-2 control-label font-title">验证:</label>
-                            <font class="col-md-10 verification1" style="">
-                                <font class="l-captcha" data-site-key="{{ env('CAPTCHA_SITE_KEY') }}" data-width="100%" style="display:inline-block ; border-radius:10px;"></font>
-                            </font>
+                        <div class="form-group">
+                            <label class="col-md-2 control-label">验证</label>
+                            <div class="col-md-10">
+                                <div class="l-captcha" data-site-key="{{ env('CAPTCHA_SITE_KEY') }}" data-width="100%"></div>
+                            </div>
                         </div>
                         @endif
-                        <div><input type="checkbox" class="checkbox">
-                            <label>
-                            <font class="rememberPassword" name="remember">记住密码</font></label></div>
-                    <div class="logIn">
-                        <button type="submit" class="button">登录</button>
-                        <font class="weChat"><a href="{{ url('/auth/register') }}">注册</a></font>
-                        <font class="weChat"><a href="{{ url('/password/phone') }}">忘记密码</a></font>
-                        <font class="weChat">
-                        <img src="{{ asset('/images/icon24_appwx_logo.png') }}"
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label"></label>
+                            <div class="col-md-10">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember"> 记住我
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-2 control-label"></label>
+                            <div class="col-md-10">
+                                <button type="submit" class="btn btn-primary">登录</button>
+                                <a class="btn btn-link" href="{{ url('/password/phone') }}">忘记密码</a>
+                                <a class="btn btn-link" href="{{ url('/auth/register') }}">注册账户</a>
+                                <img style="cursor:pointer; padding-left:20px;" src="{{ asset('/images/icon24_appwx_logo.png') }}"
                                     onclick="javascript:window.location.href='<?php echo
                                     "https://open.weixin.qq.com/connect/qrconnect?appid=" . env('WECHAT_APPID') .
                                     "&redirect_uri=" . urlencode(url('oauth/wechatCallback?goto=/auth/login')) .
                                     "&response_type=code&scope=snsapi_login&state=" . md5(time()) .
                                     "#wechat_redirect"; ?>'">
-                        </font>
-                    </div>
-                </form>
-            </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
 
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
- <div class="arrow_down"> <a href="#section6"> <img src="/images/arrow_down.png" width="42px" height="24px" alt="arrow_down"> </a> </div>
-  </article>
-</section>
-<section id="section6" class="section section6">
-  <article class="sectionWrapper section6Wrapper">
-  <div class="">
-        <p class="details-title">嘉禄讯汇&nbsp;&nbsp;内部系统</p></div>
-<div class="flash4">
-    <ul class="ul">
-        <li class="first" style="width: 280px; height:405px; overflow: hidden;">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/db"><p class="bt_1">数据库</p><p class="bt_2" ></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/db"><img src="/images/mysql.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/wiki"><p class="bt_1">Wiki</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/wiki"><img src="/images/wiki.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/book "><p class="bt_1">图书馆</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/book "><img src="/images/wordpress.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/redis"><p class="bt_1">Redis</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/redis"><img src="/images/redis.png" class="image"></a></div>
-        </li>
-    </ul>   
-</div>
-  </article>
-</section>
-
-</body>
-</html>
+@endsection
--- resources/views/oauth/authorize.blade.php
+++ resources/views/oauth/authorize.blade.php
@@ -1,130 +1,84 @@
-<!DOCTYPE HTML>
-<html>
-<head>  
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title></title>
-<link rel="stylesheet" type="text/css" href="{{ asset('/css/login.css') }}"> 
-<script src="{{ asset('/js/jquery.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/login.js') }}" type="text/javascript"></script>
-<script src="{{ asset('/js/jquery.mousewheel.min.js') }}"  type="text/javascript"></script>
-<script type="text/javascript">
+@extends('oauth.base')
 
-</script> 
-</head>
-<body>
-<div class="top section"></div> 
-<!-- container -->
-<div id="formbackground" style="position:absolute; width:100%; height:100%; z-index:-1">  
-<img src="" height="100%" width="100%" class="backgroundimg"/>  
-</div>
-<section id="section1" class="section section1">
-  <article class="sectionWrapper section1Wrapper fadeInDown">
-      <div class="container">
-      
-    <div class="login">
-        <div class="login-first">
-            <div class="login-font1">
-                <font style="margin-left:10px; color:#232323">
-                    <a class="navbar-brand" href="{{ url('/') }}">{{ Cache::get('settings:site_name') }}</a>
-                    <a class="navbar-brand" href="{{ url('/') }}">{{ $client->getName() }}</a>
-                </font>
-            </div>
-            <div class="login-font">统一身份认证</div>
-            @if (count($errors) > 0)
-                <div class="alert alert-danger  danger-warning">
-                    <strong>Whoops!</strong> There were some problems with your input.
-                    <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-                </ul>
-        </div>
-        @endif
-            <div class="login-input">
-                <form  role="form" method="post" action="{{ route('oauth.authorize.post', $params) }}">
-                    <input type="hidden" name="client_id" value="{{$params['client_id']}}">
-                    <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
-                    <input type="hidden" name="response_type" value="{{$params['response_type']}}">
-                    <input type="hidden" name="state" value="{{$params['state']}}">
-                    <input type="hidden" name="scope" value="{{$params['scope']}}">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                    <div><font class="font">账号:</font>
-                        <input type="text" class="userName"  name="username" value="{{ old('username') }}" placeholder="用户名/邮箱/手机" style="margin-top:1.5%;"></div>
-                    <div><font class="font">密码:</font>
-                        <input type="password" class="password"  name="password" placeholder="密码"></div>
-                   
-                    <p class="logIn">
-                        <button type="submit" class="button" value=1 name="approve">登录</button>
-                        <button type="submit" class="button" value=0  name="approve">取消</button>
-                        <font class="weChat"><a href="{{ url('/auth/register') }}">注册</a></font>
-                        <font class="weChat"><a href="{{ url('/password/phone') }}">忘记密码</a></font>
-                        <font class="weChat">
-                        <img src="{{ asset('/images/icon24_appwx_logo.png') }}"
-                                    onclick="javascript:window.location.href='<?php
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">统一身份认证</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" method="post" role="form" action="{{ route('oauth.authorize.post', $params) }}">
+                        <input type="hidden" name="client_id" value="{{$params['client_id']}}">
+                        <input type="hidden" name="redirect_uri" value="{{$params['redirect_uri']}}">
+                        <input type="hidden" name="response_type" value="{{$params['response_type']}}">
+                        <input type="hidden" name="state" value="{{$params['state']}}">
+                        <input type="hidden" name="scope" value="{{$params['scope']}}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label class="col-md-2 control-label"></label>
+                                <div class="col-md-10">
+                                    <p class="lead">{{ $client->getName() }}</p>
+                                    将访问您的如下资料：
+                                    <ul>
+                                    <li class="text-success">个人信息</li>
+                                </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">账户</label>
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="用户名/邮箱/手机">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">密码</label>
+                                <div class="col-md-10">
+                                    <input type="password" class="form-control" name="password" placeholder="密码">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-2 control-label"></label>
+                                <div class="col-md-10">
+                                    <button type="submit" name="approve" value=1 class="btn btn-primary">登录</button>
+                                    <button type="submit" name="approve" value=0 class="btn btn-primary">取消</button>
+                                </div>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label"></label>
+                                <div class="col-md-10">
+                                    <a class="btn btn-link" href="{{ url('/password/phone') }}">忘记密码</a>
+                                    <a class="btn btn-link" href="{{ url('/auth/register') }}">注册账户</a>
+                                    <img style="cursor:pointer; padding-left:20px;" src="{{ asset('/images/icon24_appwx_logo.png') }}"
+                                        onclick="javascript:window.location.href='<?php
                                             $wechatLoginA = "https://open.weixin.qq.com/connect/qrconnect?appid=" . env('WECHAT_APPID') .
                                                 "&redirect_uri=" . urlencode(url('oauth/wechatCallback?goto=' . $_GET['redirect_uri']));
                                             $wechatLoginB = "&response_type=code&scope=snsapi_login&state=" . md5(time()) .
                                                 "#wechat_redirect";
                                             echo $wechatLoginA . ('&failed=' . $wechatLoginA . $wechatLoginB) . $wechatLoginB;
                                         ?>'">
-                        </font>
-                    </p>
-                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-
         </div>
     </div>
 </div>
-  <div class="arrow_down"> <a href="#section6"> <img src="/images/arrow_down.png" width="42px" height="24px" alt="arrow_down"> </a> </div>
-  </article>
-</section>
-<section id="section6" class="section section6">
-  <article class="sectionWrapper section6Wrapper">
-  <div class="">
-        <p class="details-title">嘉禄讯汇&nbsp;&nbsp;内部系统</p></div>
-<div class="flash4">
-    <ul class="ul">
-        <li class="first" style="width: 280px; height:405px; overflow: hidden;">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-             <div class="imgBot">
-                <a href="http://www.szjlxh.com/db"><p class="bt_1">数据库</p><p class="bt_2" ></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/db"><img src="/images/mysql.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/wiki"><p class="bt_1">Wiki</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/wiki"><img src="/images/wiki.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/book "><p class="bt_1">图书馆</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/book "><img src="/images/wordpress.png" class="image"></a></div>
-        </li>
-        <li style="width: 260px; overflow: hidden; height:350px"">
-            <div class="imgTop">
-                <div style="width:278px;height:405px;" alt="" class="tm"></div>
-            </div>
-            <div class="imgBot">
-                <a href="http://www.szjlxh.com/redis"><p class="bt_1">Redis</p><p class="bt_2"></p></a>
-            </div>
-            <div class="imgDiv"><a href="http://www.szjlxh.com/redis"><img src="/images/redis.png" class="image"></a></div>
-        </li>
-    </ul>   
-</div>
-  </article>
-</section>
-
-</body>
-</html>
+@endsection
